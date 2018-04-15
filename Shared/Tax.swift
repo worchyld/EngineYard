a28b61f5a6dd212d3d43455872d7757ef4d48a81
AD <silentflute.ad@gmail.com>
2018-04-15 11:14:06 +0100

    add boundary check

--- Shared/Tax.swift
+++ Shared/Tax.swift
@@ -14,20 +14,29 @@ struct Tax
 
     // Subtract tax from a given balance
     public static func pay(onBalance: Int) -> Int {
-        return Int(onBalance - self.due(onBalance: onBalance))
+        if (onBalance > 0) {
+            return Int(onBalance - self.due(onBalance: onBalance))
+        }
+        return 0
     }
 
     // Tax is always rounded down (as per game rules)
     public static func due(onBalance: Int) -> Int {
-        return Int(floor(Float(onBalance) * Tax.rate))
+        if (onBalance > 0) {
+            return Int(floor(Float(onBalance) * Tax.rate))
+        }
+        return 0
     }
 
     // MARK: (Private)
 
     // Add tax to a given balance
     public static func add(toBalance: Int) -> Int {
-        let taxDue: Int = self.due(onBalance: toBalance)
-        return Int(toBalance + taxDue)
+        if (toBalance > 0) {
+            let taxDue: Int = self.due(onBalance: toBalance)
+            return Int(toBalance + taxDue)
+        }
+        return 0
     }
 
 }
