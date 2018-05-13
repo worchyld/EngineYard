e2bdc1305455d4c0dba18114df98d5c06e3667a8
AD <silentflute.ad@gmail.com>
2018-05-13 20:17:44 +0100

    Update

---
+++ Shared/GameSetupManager.swift
@@ -0,0 +1,17 @@
+//
+//  GameSetupManager.swift
+//  EngineYard
+//
+//  Created by Amarjit on 13/05/2018.
+//  Copyright © 2018 Amarjit. All rights reserved.
+//
+
+import Foundation
+
+final class GameSetupManager {
+    static var instance = GameSetupManager()
+
+    func setup(players: [Player]) throws -> Game? {
+        return nil
+    }
+}
