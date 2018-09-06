71235bfbd3ed445caeadb7168ce4c38edce1f669
AD <silentflute.ad@gmail.com>
2018-09-06 10:14:47 +0100

    Use tuples instead

--- EngineYardTests/SalesTests.swift
+++ EngineYardTests/SalesTests.swift
@@ -22,44 +22,13 @@ class SalesTests: EngineYardTests {
         super.tearDown()
     }
 
-    /*
-    func testLower() {
-        let orders = [6]
-        let units = 1
-
-        let rule = SalesRules(orders)
-
-        if let perfectMatch = rule.perfectMatch(units) {
-            print("Found perfect match for: \(units) in orders \(rule.orders) at index: \(perfectMatch.0) which is the value \(perfectMatch.1)")
-            XCTFail("Expected lower, got perfect match")
-        }
-        else {
-            if let lowerMatch = rule.lowerMatch(units) {
-                print("Found lower match for: \(units) in orders \(rule.orders) at index: \(lowerMatch.0) which is the value \(lowerMatch.1)")
-            }
-            else {
-                if let higherMatch = rule.higherMatch(units) {
-                    print("Found higher match for: \(units) in orders \(rule.orders) at index: \(higherMatch.0)  which is the value \(higherMatch.1)")
-                    XCTFail("Expected lower, got higher match")
-                }
-                else {
-                    print ("No match found")
-                    XCTFail("Expected lower, Got no match")
-                }
-            }
-        } // end-if
-    }
-     */
-
     func testPerfectMatch() {
         let orders = [6]
         let units = 6
 
         let handler = SalesRuleHandler(orders: orders, units: units)
 
-        XCTAssertEqual(handler.matchType, MatchType.perfectMatch)
-        XCTAssertEqual(handler.matchTuple!.0, 0)
-        XCTAssertEqual(handler.matchTuple!.1, 6)
+        XCTAssertEqual(handler.matchType!, MatchType.perfectMatch(0, 0))
     }
 
     func testLowerMatch() {
@@ -68,21 +37,16 @@ class SalesTests: EngineYardTests {
 
         let handler = SalesRuleHandler(orders: orders, units: units)
 
-        XCTAssertEqual(handler.matchType, MatchType.lowerMatch)
-        XCTAssertEqual(handler.matchTuple!.0, 0)
-        XCTAssertEqual(handler.matchTuple!.1, 6)
+        XCTAssertEqual(handler.matchType!, MatchType.lowerMatch(0, 0))
     }
 
-
-    func testHigher() {
+    func testHigherMatch() {
         let orders = [1]
         let units = 3
 
         let handler = SalesRuleHandler(orders: orders, units: units)
 
-        XCTAssertEqual(handler.matchType, MatchType.higherMatch)
-        XCTAssertEqual(handler.matchTuple!.0, 0)
-        XCTAssertEqual(handler.matchTuple!.1, 1)
+        XCTAssertEqual(handler.matchType!, MatchType.higherMatch(0, 0))
     }
 
 }
--- Shared/Locomotive/SalesRuleHandler.swift
+++ Shared/Locomotive/SalesRuleHandler.swift
@@ -9,9 +9,9 @@
 import Foundation
 
 enum MatchType : Equatable {
-    case perfectMatch
-    case lowerMatch
-    case higherMatch
+    case perfectMatch(Int, Int)
+    case lowerMatch(Int, Int)
+    case higherMatch(Int, Int)
 }
 
 extension MatchType {
@@ -34,37 +34,33 @@ class SalesRuleHandler {
 
     private var orders: [Int]
     private var units: Int
-    public private(set) var matchType: MatchType?
-    public private(set) var matchTuple: (Int, Int)?
+    var matchType: MatchType?
 
     init(orders: [Int], units: Int) {
         self.orders = orders
         self.units = units
-        self.handler()
+        self.matchType = self.handler()
     }
 
-    private func handler() {
+    private func handler() -> MatchType? {
         let rule = SalesRules(orders)
 
         if let match = rule.perfectMatch(units) {
             print("Found perfect match for: \(units) in orders \(rule.orders) at index: \(match.0) which is the value \(match.1)")
-            self.matchType = MatchType.perfectMatch
-            self.matchTuple = match
+            return MatchType.perfectMatch(match.0, match.1)
         }
         else {
             if let match = rule.lowerMatch(units) {
                 print("Found lower match for: \(units) in orders \(rule.orders) at index: \(match.0) which is the value \(match.1)")
-                self.matchType = MatchType.lowerMatch
-                self.matchTuple = match
+                return MatchType.lowerMatch(match.0, match.1)
             }
             else {
                 if let match = rule.higherMatch(units) {
-                    print("Found higher match for: \(units) in orders \(rule.orders) at index: \(match.0)  which is the value \(match.1)")
-                    self.matchType = MatchType.higherMatch
-                    self.matchTuple = match
+                    return MatchType.higherMatch(match.0, match.1)
                 }
             }
-        } // end-if
+        }
+        return nil
     }
 
 }
