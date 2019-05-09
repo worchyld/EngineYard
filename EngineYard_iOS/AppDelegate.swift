eca17e9933517608e5507fbe4bc98bf7d3f437dc
AD <silentflute.ad@gmail.com>
2019-05-09 06:18:54 +0100

    remove file, add save context

--- EngineYard_iOS/AppDelegate.swift
+++ EngineYard_iOS/AppDelegate.swift
@@ -40,6 +40,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        // Saves changes in the application's managed object context before the application terminates.
+        self.saveContext()
     }
 
     // MARK: - Core Data stack
@@ -51,7 +53,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
          application to it. This property is optional since there are legitimate
          error conditions that could cause the creation of the store to fail.
          */
-        let container = NSPersistentContainer(name: "_23")
+        let container = NSPersistentContainer(name: "EngineYard")
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
                 // Replace this implementation with code to handle the error appropriately.
