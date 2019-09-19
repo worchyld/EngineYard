9abd0dfdb079a3a42d7b897cb4138763984cd55f
AD <silentflute.ad@gmail.com>
2019-09-19 23:05:40 +0100

    Remove enums file. Update card state file

---
+++ Shared/Models/CardState.swift
@@ -0,0 +1,20 @@
+//
+//  CardState.swift
+//  EngineYard
+//
+//  Created by Amarjit on 19/09/2019.
+//  Copyright © 2019 Amarjit. All rights reserved.
+//
+
+import Foundation
+
+// Obsolescence
+enum CardState : Int {
+    case inactive = 1
+    case active = 2
+    case obsolete = -1
+
+    static let allRawValues = CardState.inactive.rawValue...CardState.obsolete.rawValue
+    static let allValues = Array(allRawValues.map{ CardState(rawValue: $0)! })
+}
+
