b8a444d0dd09f65928a66bb7d3489e0085952101
AD <silentflute.ad@gmail.com>
2019-09-19 22:44:43 +0100

    [Pre-swift 5 updates] - Clearing out unfinished stuff

--- EngineYardTests/EngineYardTests.swift
+++ EngineYardTests/EngineYardTests.swift
@@ -9,30 +9,6 @@
 import XCTest
 @testable import EngineYard
 
-
-extension Mock {
-
-    struct Cards {
-        var green: Int = 0
-        var red: Int = 0
-        var blue: Int = 0
-        var yellow: Int = 0
-        var total : Int {
-            return (self.green + self.red + self.blue + self.yellow)
-        }
-
-        struct Expected {
-            static let green = Rules.Board.numberOfCardsForColor(engineColor: .green)
-            static let red = Rules.Board.numberOfCardsForColor(engineColor: .red)
-            static let blue = Rules.Board.numberOfCardsForColor(engineColor: .blue)
-            static let yellow = Rules.Board.numberOfCardsForColor(engineColor: .yellow)
-            static let total = Rules.Board.cards
-        }
-    }
-}
-
-
-
 class EngineYardTests: XCTestCase {
     
     override func setUp() {
@@ -43,20 +19,6 @@ class EngineYardTests: XCTestCase {
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
-    }
-
-    func prepare() -> Board? {
-        let players = Mock.players(howMany: 5)
-
-        guard let game:Game = Game.setup(with: players) else {
-            XCTFail("Game object did not initialise")
-            return nil
-        }
-        guard let board = game.board else {
-            XCTFail("Board object not initialised")
-            return nil
-        }
-        return board
     }
 
 }
