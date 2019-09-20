e8171203ba710621f447fc5c15af16cabdf58896
AD <silentflute.ad@gmail.com>
2019-09-20 10:27:07 +0100

    Attempt at making co-ordinators to manage app flow

---
+++ EngineYard_iOS/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  EngineYard
+//
+//  Created by Amarjit on 20/09/2019.
+//  Copyright © 2019 Amarjit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, Storyboarded {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
