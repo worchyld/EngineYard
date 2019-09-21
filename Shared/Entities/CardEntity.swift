bff78a23c6de07fec3d3c6b450a0d298d6b15381
AD <silentflute.ad@gmail.com>
2019-09-21 19:55:03 +0100

    Add entity folder, intention for this is to hold db entities.

---
+++ Shared/Entities/CardEntity.swift
@@ -0,0 +1,11 @@
+//
+//  CardEntity.swift
+//  EngineYard
+//
+//  Created by Amarjit on 21/09/2019.
+//  Copyright © 2019 Amarjit. All rights reserved.
+//
+
+import Foundation
+
+// #TODO
--- Shared/Models/Deck.swift
+++ Shared/Models/Deck.swift
@@ -25,6 +25,37 @@ protocol DeckProtocol {
     var owners : Int { get }
 }
 
-final class Deck {
+final class Deck : DeckProtocol {
+    public private (set) var cards: [Card] = [Card]()
 
+    public private (set) var name: String
+    public private (set) var type: DeckType
+    public private (set) var generation: Generation
+    public private (set) var cost: Int
+    public private (set) var productionCost: Int
+    public private (set) var income: Int
+    public private (set) var capacity: Int
+    public private (set) var numberOfChildren: Int
+    public internal (set) var state: CardState
+    public private (set) var owners: Int
+
+    init(name: String, type: DeckType, generation: Generation, cost: Int, capacity: Int, numberOfChildren: Int) {
+        assert(cost % 4 == 0, "Cost must be a multiple of 4")
+        self.name = name
+        self.type = type
+        self.generation = generation
+        self.cost = cost
+        self.productionCost = (cost / 2)
+        self.income = (productionCost / 2)
+        self.capacity = capacity
+        self.numberOfChildren = numberOfChildren
+        self.state = .inactive
+        self.owners = 0
+    }
+}
+
+extension Deck : CustomStringConvertible {
+    var description: String {
+        return (self.name)
+    }
 }
