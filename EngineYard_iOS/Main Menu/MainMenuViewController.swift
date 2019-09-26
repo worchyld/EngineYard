5465f382e1ed5ba73db1675de49e7c7c3db016e1
AD <silentflute.ad@gmail.com>
2019-09-26 21:40:09 +0100

    Adding some xibs.  they are not connected to anything just yet

---
+++ EngineYard_iOS/Main Menu/MainMenuViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MainMenuViewController.swift
+//  EngineYard
+//
+//  Created by Amarjit on 26/09/2019.
+//  Copyright © 2019 Amarjit. All rights reserved.
+//
+
+import UIKit
+
+class MainMenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
