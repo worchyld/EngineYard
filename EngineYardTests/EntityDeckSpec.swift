a7d2b4b895c202bef263c789ec9628e6391bdcfb
AD <silentflute.ad@gmail.com>
2020-04-20 07:22:02 +0100

    adding test to see if realm will save a single deck entry

---
+++ EngineYardTests/EntityDeckSpec.swift
@@ -0,0 +1,66 @@
+//
+//  EntityDeckSpec.swift
+//  EngineYardTests
+//
+//  Created by Amarjit on 18/04/2020.
+//  Copyright © 2020 Amarjit. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+import XCTest
+import Quick
+import Nimble
+
+@testable import EngineYard
+
+// [REALM] Testing `Deck` entity
+
+class EntityDeckSpec: BaseSpec {
+
+    override func spec() {
+        super.spec()
+
+        describe("models") {
+
+            describe("initialize with name and age") {
+                it("initializes correctly") {
+                    let deck = Deck.init(name: "Deck #1", cost: 4, generation: .first, color: .green, capacity: 4, numberOfChildren: 4)
+                    let deckEntity = DeckEntity.init(with: deck)
+
+                    expect(deckEntity.name) == deck.name
+                    expect(deckEntity.state) == deck.state.rawValue
+                    expect(deckEntity.generation) == deck.generation.rawValue
+                    expect(deckEntity.color) == deck.color.rawValue
+                    expect(deckEntity.cost) == deck.cost
+                    expect(deckEntity.capacity) == deck.capacity
+                    expect(deckEntity.numberOfChildren) == deck.numberOfChildren
+                }
+            }
+
+            describe("attempting saving deck") {
+                it("saves single deck correctly") {
+                    let deck = Deck.init(name: "Deck #1", cost: 4, generation: .first, color: .green, capacity: 4, numberOfChildren: 4)
+                    let deckEntity = DeckEntity.init(with: deck)
+
+                    deckEntity.save()
+                    let realm = try! Realm()
+
+                    guard let deckFromDatabase = realm.objects(DeckEntity.self).last else {
+                        fail("No deck from database found")
+                        return
+                    }
+
+                    expect(deckFromDatabase.name) == deck.name
+                    expect(deckFromDatabase.state) == deck.state.rawValue
+                    expect(deckFromDatabase.generation) == deck.generation.rawValue
+                    expect(deckFromDatabase.color) == deck.color.rawValue
+                    expect(deckFromDatabase.cost) == deck.cost
+                    expect(deckFromDatabase.capacity) == deck.capacity
+                    expect(deckFromDatabase.numberOfChildren) == deck.numberOfChildren
+                }
+            }
+
+        }
+    }
+}
--- Shared/Entities/DeckEntity.swift
+++ Shared/Entities/DeckEntity.swift
@@ -16,12 +16,50 @@ class DeckEntity : Object {
     @objc dynamic var generation: Int = 0
     @objc dynamic var name: String = ""
     @objc dynamic var cost: Int = 0
-    @objc dynamic var productionCost: Int = 0
-    @objc dynamic var income: Int = 0
     @objc dynamic var capacity: Int = 0
     @objc dynamic var numberOfChildren: Int = 0
 
     // RLMRelationships
     let cards = List<CardEntity>() // To-many relationship
     let orders = List<OrderEntity>() // To-many relationship
+
+    convenience init(with deck: Deck) {
+        self.init()
+        self.state = deck.state.rawValue
+        self.color = deck.color.rawValue
+        self.generation = deck.generation.rawValue
+        self.name = deck.name
+        self.cost = deck.cost
+        self.capacity = deck.capacity
+        self.numberOfChildren = deck.numberOfChildren
+    }
+}
+
+
+extension DeckEntity {
+  func save() {
+    let realm = try! Realm()
+    try! realm.write {
+      realm.add(self)
+    }
+  }
+
+  class func all() -> Results<DeckEntity> {
+    let realm = try! Realm()
+    return realm.objects(DeckEntity.self)
+  }
+
+  func updateFrom(deckEntity: DeckEntity) {
+    let realm = try! Realm()
+    try! realm.write {
+        realm.add(deckEntity, update: .modified)
+    }
+  }
+
+  func delete() {
+    let realm = try! Realm()
+    try! realm.write {
+      realm.delete(self)
+    }
+  }
 }
