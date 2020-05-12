a931252c54f7ade77233b51bcddc9e634dbe7098
AD <silentflute.ad@gmail.com>
2020-05-12 21:04:41 +0100

    Restore original deck entity class

--- Shared/Entities/DeckEntity.swift
+++ Shared/Entities/DeckEntity.swift
@@ -11,11 +11,28 @@ import RealmSwift
 
 // :[Realm entity] Deck
 
-class DeckEntity: Object {
-    @objc dynamic var uuid: UUID = UUID()
-    @objc dynamic var id: Int = 0
+class DeckEntity : Object {
+    @objc dynamic var id = UUID().uuidString
+    @objc dynamic var state: Int = 0
+    @objc dynamic var color: Int = 0
+    @objc dynamic var generation: Int = 0
+    @objc dynamic var name: String = ""
+    @objc dynamic var cost: Int = 0
+    @objc dynamic var capacity: Int = 0
+    @objc dynamic var numberOfChildren: Int = 0
 
     override static func primaryKey() -> String {
        return "id"
+    }
+
+    // RLMRelationships
+    let cards = List<CardEntity>() // To-many relationship
+    let orders = List<OrderEntity>() // To-many relationship
+}
+
+extension DeckEntity {
+    override var debugDescription: String {
+        let string = "ID: \(self.id), name: \(self.name), cost: \(self.cost), generation:\(self.generation), color: \(self.color), capacity: \(self.capacity), numberOfChildren: \(self.numberOfChildren), #cards: \(self.cards.count)"
+        return string
     }
 }
