750b81a226f063e40de77383d7ad309bd76a0d73
AD <silentflute.ad@gmail.com>
2020-05-16 11:36:34 +0100

    adding code for saving to realm

--- EngineYardTests/RealmGameModelSaveTests.swift
+++ EngineYardTests/RealmGameModelSaveTests.swift
@@ -16,16 +16,40 @@ import GameplayKit
 
 class RealmGameModelSaveTests: XCTestCase {
 
+    let inMemoryIdentifier: String = "Yard"
+
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        Realm.Configuration.defaultConfiguration.inMemoryIdentifier = inMemoryIdentifier
+        print("[REALM]: \(Realm.Configuration.defaultConfiguration.description)")
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        let realm = try! Realm()
+        try! realm.write {
+            realm.deleteAll()
+        }
     }
 
-    func testDidSaveState() {
+    func testRealmHasMemoryIdentifier() {
+        let config = Realm.Configuration.defaultConfiguration
+        XCTAssertTrue(config.inMemoryIdentifier == inMemoryIdentifier)
+    }
+
+    func testRealmDoesConnect() {
+        do {
+            let realm = try Realm()
+            XCTAssertNotNil(realm)
+        }
+        catch {
+            XCTFail("Realm connection Error - \(error.localizedDescription)")
+        }
+    }
+
+    func testDidSaveGameModel() {
         
     }
 
 }
+
