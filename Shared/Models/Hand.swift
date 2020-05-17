0321b15966639d6ecb7b74b0e741ba2da7b0cfc7
AD <silentflute.ad@gmail.com>
2020-05-17 17:49:32 +0100

    Adding a opening class for hand (players hand of cards)

---
+++ Shared/Models/Hand.swift
@@ -0,0 +1,14 @@
+//
+//  Hand.swift
+//  EngineYard
+//
+//  Created by Amarjit on 17/05/2020.
+//  Copyright © 2020 Amarjit. All rights reserved.
+//
+
+import Foundation
+
+// A player has a hand of cards, ie: Their portfolio of locomotives they own
+class Hand {
+    var cards: [Card] = [Card]() // considered empty at initialisation
+}
