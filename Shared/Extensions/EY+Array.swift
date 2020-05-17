6613f0e39a7b86b2ab4d364acfb24c5152846b1c
AD <silentflute.ad@gmail.com>
2020-05-17 13:17:27 +0100

    add a mutater array extension

--- Shared/Extensions/EY+Array.swift
+++ Shared/Extensions/EY+Array.swift
@@ -82,3 +82,12 @@ extension Array where Element: Hashable {
         return Array(thisSet.symmetricDifference(otherSet))
     }
 }
+extension Array {
+    mutating func mutateEach(by transform: (inout Element) throws -> Void) rethrows {
+        self = try map { el in
+            var el = el
+            try transform(&el)
+            return el
+        }
+     }
+}
