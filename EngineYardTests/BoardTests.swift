129fc8c724a535c08681c8882d0ceabdc76e2c73
AD <silentflute.ad@gmail.com>
2020-05-17 13:28:10 +0100

    Adding 2 simple tests against the board struct

---
+++ EngineYardTests/BoardTests.swift
@@ -0,0 +1,32 @@
+//
+//  BoardTests.swift
+//  EngineYardTests
+//
+//  Created by Amarjit on 17/05/2020.
+//  Copyright © 2020 Amarjit. All rights reserved.
+//
+
+import XCTest
+
+@testable import EngineYard
+
+class BoardTests: XCTestCase {
+
+    private var board: Board!
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        self.board = Board()
+    }
+
+    func testBoardHasExpectedNumberOfPositions() {
+        XCTAssertTrue(board.positions.count == Constants.totalBoardPositions)
+    }
+
+    func testBoardHasExpectedNumberOfCards() {
+        let allCards = board.getAllCards()
+        XCTAssertTrue(allCards.count == Constants.totalCards)
+    }
+    
+
+}
