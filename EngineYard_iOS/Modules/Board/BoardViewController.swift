6368f54b976219f7716f64df6bc2beba8377f6b2
AD <silentflute.ad@gmail.com>
2020-05-29 20:42:28 +0100

    add new storyboard to hold board info

---
+++ EngineYard_iOS/Modules/Board/BoardViewController.swift
@@ -0,0 +1,31 @@
+//
+//  BoardViewController.swift
+//  EngineYard
+//
+//  Created by Amarjit on 29/05/2020.
+//  Copyright © 2020 Amarjit. All rights reserved.
+//
+
+import UIKit
+
+class BoardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.title = "Board"
+        self.view.backgroundColor = .systemPink
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
