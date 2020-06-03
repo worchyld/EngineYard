e3bd743f8f3c2ca47894ba9f94f05d30e3d07b1d
AD <silentflute.ad@gmail.com>
2020-06-03 15:21:49 +0100

    created outlets for each of the labels

--- EngineYard_iOS/XIBs/CardView.swift
+++ EngineYard_iOS/XIBs/CardView.swift
@@ -10,6 +10,21 @@ import UIKit
 
 class CardView: UIView {
 
+    @IBOutlet weak var trainIconView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var generationLabel: UILabel!
+    @IBOutlet weak var costLabel: UILabel!
+    @IBOutlet weak var productionLabel: UILabel!
+    @IBOutlet weak var incomeLabel: UILabel!
+    @IBOutlet var iconSidebarCollection: [UIImageView]!
+    @IBOutlet weak var ordersLabel: UILabel!
+    @IBOutlet var diceImageViewCollection: [UIImageView]!
+    @IBOutlet weak var buyButton: UIButton!
+
+    @IBAction func buyButtonDidPress(_ sender: UIButton) {
+        print ("Do something here")
+    }
+
     /*
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
