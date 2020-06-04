7da1a5d689b78ca75506de40181e82563f022dfe
AD <silentflute.ad@gmail.com>
2020-06-04 06:28:30 +0100

    making a new card view almost full of stackviews

---
+++ EngineYard_iOS/XIBs/NewCardView.swift
@@ -0,0 +1,15 @@
+//
+//  NewCardView.swift
+//  EngineYard
+//
+//  Created by Amarjit on 04/06/2020.
+//  Copyright © 2020 Amarjit. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class NewCardView: UIView {
+    
+}
