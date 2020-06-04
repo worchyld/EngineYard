ca3f0e5aa11806900a84e215ceb736c815589bed
AD <silentflute.ad@gmail.com>
2020-06-04 10:09:52 +0100

    reverting card view

--- EngineYard_iOS/Modules/Board/CardDetailViewController.swift
+++ EngineYard_iOS/Modules/Board/CardDetailViewController.swift
@@ -22,5 +22,13 @@ class CardDetailViewController: UIViewController, NibViewController, Storyboarde
         super.viewDidLoad()
         self.title = "Card view"
         self.cardView.layoutIfNeeded()
+
+        NSLayoutConstraint.activate([
+            cardView.widthAnchor.constraint(equalToConstant: 155),
+            cardView.heightAnchor.constraint(equalToConstant: 200),
+            cardView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            cardView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor)
+        ])
+        self.view.setNeedsDisplay()
     }
 }
