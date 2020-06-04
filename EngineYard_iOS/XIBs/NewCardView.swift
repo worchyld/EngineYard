d9f7b5aa27321911fec67e55151113cc809b89f4
AD <silentflute.ad@gmail.com>
2020-06-04 06:46:57 +0100

    hook up just the header stack view and inject a background color for now

--- EngineYard_iOS/XIBs/NewCardView.swift
+++ EngineYard_iOS/XIBs/NewCardView.swift
@@ -12,4 +12,25 @@ import UIKit
 
 class NewCardView: UIView {
     
+    @IBOutlet weak var headerStackView: UIStackView!
+
+    override func draw(_ layer: CALayer, in ctx: CGContext) {
+        super.draw(layer, in: ctx)
+
+        let backgroundView = UIView()
+        backgroundView.backgroundColor = UIColor.purple
+
+        backgroundView.translatesAutoresizingMaskIntoConstraints = false
+
+        // put background view as the most background subviews of stack view
+        headerStackView.insertSubview(backgroundView, at: 0)
+
+        // pin the background view edge to the stack view edge
+        NSLayoutConstraint.activate([
+            backgroundView.leadingAnchor.constraint(equalTo: headerStackView.leadingAnchor),
+            backgroundView.trailingAnchor.constraint(equalTo: headerStackView.trailingAnchor),
+            backgroundView.topAnchor.constraint(equalTo: headerStackView.topAnchor),
+            backgroundView.bottomAnchor.constraint(equalTo: headerStackView.bottomAnchor)
+        ])
+    }
 }
