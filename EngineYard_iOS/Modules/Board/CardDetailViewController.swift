5e6c21b15052283ad5fa21b59683a64f2865f4cf
AD <silentflute.ad@gmail.com>
2020-06-05 00:59:06 +0100

    adding some quick fillers

--- EngineYard_iOS/Modules/Board/CardDetailViewController.swift
+++ EngineYard_iOS/Modules/Board/CardDetailViewController.swift
@@ -14,22 +14,85 @@ class CardDetailViewController: UIViewController, Storyboarded {
     weak var coordinator: BoardCoordinator?
     var viewModel: CardViewModel!
 
+    weak var cardDetailView: CardDetailView?
+
+    lazy var formattedCost: String? = {
+        guard let viewModel = self.viewModel else {
+            return nil
+        }
+        let cost = viewModel.card.cost
+        let costAsNumber = NSNumber(value: cost)
+        let cache = NumberFormatCache.currencyRateFormatter
+        return cache.string(from: costAsNumber) ?? "NaN"
+    }()
+    lazy var formattedProductionCost: String? = {
+        guard let viewModel = self.viewModel else {
+            return nil
+        }
+        let cost = viewModel.card.productionCost
+        let costAsNumber = NSNumber(value: cost)
+        let cache = NumberFormatCache.currencyRateFormatter
+        return cache.string(from: costAsNumber) ?? "NaN"
+    }()
+    lazy var formattedIncome: String? = {
+        guard let viewModel = self.viewModel else {
+            return nil
+        }
+        let cost = viewModel.card.income
+        let costAsNumber = NSNumber(value: cost)
+        let cache = NumberFormatCache.currencyRateFormatter
+        return cache.string(from: costAsNumber) ?? "NaN"
+    }()
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Card view"
 
-        let nib = CardDetailView.nib()
-        let customView = nib.instantiate(withOwner: nil, options: nil).first as! UIView
+        //self.cardDetailView?.nameLabel?.text = "Lorem ipsum"
+        guard let viewModel = self.viewModel else {
+            return
+        }
+
+        self.configure(with: viewModel.card)
+    }
+
+    override func loadView() {
+        super.loadView()
+
+        let customView = CardDetailView.instantiate()
 
         self.view.addSubview(customView)
-
         NSLayoutConstraint.activate([
-                   customView.topAnchor.constraint(equalTo: self.view.topAnchor),
-                   customView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
-                   customView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
-                   customView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
-               ])
-
-
+            customView.topAnchor.constraint(equalTo: self.view.topAnchor),
+            customView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
+            customView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+            customView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+        ])
+        self.cardDetailView = customView
     }
+
+    func configure(with card: Card) {
+        let bgColor = UIColor.flat(color: card.color.flatColor, alpha: 0.45)
+
+        guard let detailView = self.cardDetailView else {
+            return
+        }
+
+        detailView.backgroundColor = bgColor
+
+        detailView.nameLabel?.text = "\(card.name)".capitalizingFirstLetter()
+        detailView.generationLabel?.text = "\(card.generation)".capitalizingFirstLetter() + " Generation"
+
+        detailView.costLabel?.text = self.formattedCost ?? "NaN"
+        detailView.productionLabel?.text = self.formattedProductionCost ?? "NaN"
+        detailView.incomeLabel?.text = self.formattedIncome ?? "NaN"
+
+        detailView.btnBuyBtnCollection.forEach { (btn) in
+            btn.borderColor = UIColor.systemBlue
+            btn.setTitleColor(.systemBlue, for: UIControl.State.normal)
+            btn.setTitleColor(.systemGray, for: UIControl.State.disabled)
+        }
+    }
+
 }
--- EngineYard_iOS/XIBs/CardXIBs/CardDetailView.swift
+++ EngineYard_iOS/XIBs/CardXIBs/CardDetailView.swift
@@ -9,6 +9,29 @@
 import Foundation
 import UIKit
 
+extension UINib {
+    func instantiate() -> Any? {
+        return self.instantiate(withOwner: nil, options: nil).first
+    }
+}
+
+extension UIView {
+
+    static var nib: UINib {
+        return UINib(nibName: String(describing: self), bundle: nil)
+    }
+
+    static func instantiate(autolayout: Bool = true) -> Self {
+        // generic helper function
+        func instantiateUsingNib<T: UIView>(autolayout: Bool) -> T {
+            let view = self.nib.instantiate() as! T
+            view.translatesAutoresizingMaskIntoConstraints = !autolayout
+            return view
+        }
+        return instantiateUsingNib(autolayout: autolayout)
+    }
+}
+
 class CardDetailView: UIView {
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var generationLabel: UILabel!
@@ -29,4 +52,28 @@ class CardDetailView: UIView {
     @IBAction func btnBuyDidPress(_ sender: UIButton) {
         print("You pressed buy on tag: \(sender.tag)")
     }
+
+   
+    
+
+    /*
+
+    override func initialize() {
+        super.initialize()
+
+        // first: load the view hierarchy to get proper outlets
+        let name = String(describing: type(of: self))
+        let nib = UINib(nibName: name, bundle: .main)
+        nib.instantiate(withOwner: self, options: nil)
+
+        // next: append the container to our view
+        self.addSubview(self.containerView)
+        self.containerView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            self.containerView.topAnchor.constraint(equalTo: self.topAnchor),
+            self.containerView.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+            self.containerView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            self.containerView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+        ])
+    }*/
 }
