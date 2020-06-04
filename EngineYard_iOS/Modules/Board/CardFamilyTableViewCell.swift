987ea53c107340f4f0c0df7d15a0173e37947977
AD <silentflute.ad@gmail.com>
2020-06-04 10:24:23 +0100

    small xibs to hold small partials

---
+++ EngineYard_iOS/Modules/Board/CardFamilyTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CardFamilyTableViewCell.swift
+//  EngineYard
+//
+//  Created by Amarjit on 04/06/2020.
+//  Copyright © 2020 Amarjit. All rights reserved.
+//
+
+import UIKit
+
+class CardFamilyTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
