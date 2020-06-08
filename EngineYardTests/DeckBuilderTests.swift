34999a4d6c865a39e7f016bea541deeebe0a8769
AD <silentflute.ad@gmail.com>
2020-06-08 16:19:01 +0100

    add test for deck builders

---
+++ EngineYardTests/DeckBuilderTests.swift
@@ -0,0 +1,25 @@
+//
+//  DeckBuilderTests.swift
+//  EngineYardTests
+//
+//  Created by Amarjit on 08/06/2020.
+//  Copyright © 2020 Amarjit. All rights reserved.
+//
+
+import XCTest
+
+@testable import EngineYard
+
+// Tests against the `Deck` class
+
+class DeckBuilderTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    func testBuildDecksEquals14() {
+        
+    }
+
+}
