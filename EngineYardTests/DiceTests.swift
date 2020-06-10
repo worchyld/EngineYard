3c59db195925ea5d020604e34ec4d4f69266c114
AD <silentflute.ad@gmail.com>
2020-06-10 15:13:14 +0100

    remove / update files

---
+++ EngineYardTests/DiceTests.swift
@@ -0,0 +1,24 @@
+//
+//  DiceTests.swift
+//  EngineYardTests
+//
+//  Created by Amarjit on 10/06/2020.
+//  Copyright © 2020 Amarjit. All rights reserved.
+//
+
+import XCTest
+
+@testable import EngineYard
+
+class DiceTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    func testDidRollDie() {
+        let d6 = Die.roll
+        
+    }
+
+}
--- EngineYardTests/MarketDemandTests.swift
+++ EngineYardTests/MarketDemandTests.swift
@@ -22,4 +22,39 @@ class MarketDemandTests: XCTestCase {
         self.board = board
     }
 
+    // When board has been initialised
+    func testNoGreenGenerationsExist() {
+        guard let board = self.board else {
+            XCTFail("No board")
+            return
+        }
+
+        let mktDemand = MarketDemand.init(board: board)
+        let filtered = mktDemand.findDecksWithOrders(matching: .green)
+        let exists = Deck.totalGenerations(in: filtered)
+
+        XCTAssertTrue(filtered?.count == 0)
+        XCTAssertTrue(exists == 0)
+    }
+
+    func testNoGenerationsExist() {
+        guard let board = self.board else {
+            XCTFail("No board")
+            return
+        }
+
+        let mktDemand = MarketDemand.init(board: board)
+
+        for familyColor in Family.Color.allCases.enumerated() {
+            print ("testing: color \(familyColor.element)")
+
+            let filtered = mktDemand.findDecksWithOrders(matching: familyColor.element)
+            let exists = Deck.totalGenerations(in: filtered)
+
+            XCTAssertTrue(filtered?.count == 0)
+            XCTAssertTrue(exists == 0)
+        }
+
+    }
+
 }
