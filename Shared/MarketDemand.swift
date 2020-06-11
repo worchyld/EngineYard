8516f258b48d5b8709ca646b6955de9fb1d51d11
AD <silentflute.ad@gmail.com>
2020-06-11 15:33:17 +0100

    refactor market demand, add a struct to handle generations existing

--- Shared/MarketDemand.swift
+++ Shared/MarketDemand.swift
@@ -8,45 +8,160 @@
 
 import Foundation
 
-// Handles market demands phase.
+/*
+ *Market demands handler*
 
-// In this phase, new Existing Orders for marketable locomotives are determined and Existing Orders
-//   for old and obsolete locomotives are cancelled.
-//    It consists of four steps, each step dedicated to one locomotive type: (green, red, yellow, blue)
-//
+ In this phase, new existing orders for marketable locomotives
+ are determiened and existing orders for old and obsolete locomotives
+ are cancelled.
+
+ It consists of obtaining the current Market for:
+   green, red, yellow, blue
+
+ A. Determine how many generations exist for each locomotive color
+    - A generation exists if it has dice (orders) in either existing or customer base
+
+ B. Once generations exist, there are 4 cases:
+    i. One generation exists.
+       Nothing happens
+
+    ii. Two generations exists.
+        A. Starting with lower number generation (older):
+        a. Transfer one of the dice (order) from customer base (sales pool) to dice pool (remove it from play)
+        b. Roll all remaining dice in sales pool (customer base)
+        c. Move all dice from sales pool to orders pool
+        d. Mark this locomotive as old
+
+        B. The newer generation is checked.
+        a. If it does not have maximum dice (to capacity) then add 1 to the sales pool
+             then re-roll all sales pool dice then move all dice back to existing orders
+
+    iii. Three generations exists.
+        a. Start with oldest locomotive.
+        b. Remove all dice from customer base and existing orders
+        c. This locomotive is marked as obsolote
+        d. Move to next oldest, add 1 die to customer base if required
+        e. Repeat this for the newest locomotive
+ */
 
 class MarketDemand {
-    private var board: Board
+    struct GenerationsExist {
+        private var color: Family.Color
+        private var locos: [Locomotive]
+        var total: Int = 0
 
-    lazy var locos: [Locomotive]? = {
-        guard let locos = board as [Locomotive]? else {
-            return nil
+        init(with locomotives: [Locomotive], find color: Family.Color) {
+            self.locos = locomotives
+            self.color = color
         }
-        return locos
-    }()
-    private(set) var howManyGenerationsExist: Int = 0
+
+        func start() -> [Locomotive]? {
+            let results = self.marketFor(color: self.color)
+            return results
+        }
+
+        // A generation exists if it has dice (orders) in either existing or customer base
+        private func marketFor(color: Family.Color) -> [Locomotive] {
+            // Find all Locomotives that match the type & have orders,
+            // sorted by cost & generation ascending
+            let filter = locos.filter { (locomotive: Locomotive) -> Bool in
+                return (locomotive.color == color && locomotive.orders.count > 0)
+            }.sorted { (a: Locomotive, b: Locomotive) -> Bool in
+                return ((a.cost < b.cost) && (a.generation.rawValue < b.generation.rawValue))
+            }
+
+            return filter
+        }
+    }
+
+    private var board: Board
 
     init(board: Board) {
         self.board = board
     }
 
-    // Determine how many generations of that locomotive type currently exist.
-    //   A generation exists if there are dice in either the `existingOrders` (Existing Orders)
-    //   boxes or the `completedOrders` (Customer Base) boxes or in both.
+    func runReport(for color: Family.Color) -> GenerationsExist? {
+        guard let board = self.board else {
+            return nil
+        }
+        let report = GenerationsExist.init(with: board, find: color)
+        return report
+    }
 
-    func start() {
+    func start(with color: Family.Color) {
+        guard let board = self.board else {
+            return
+        }
+
+        let report = self.runReport(for: color)
+
+//        guard let filtered = report.start() else {
+//            return // do nothing
+//        }
+//
+//        let howMany = filtered.count
+//
+//        switch howMany {
+//        case 1:
+//            let results = handleOneGeneration(locos: filtered)
+//            print (results)
+//        case 2:
+//            handleTwoGenerations()
+//        case 3:
+//            handleThreeGenerations()
+//
+//        default:
+//            //print ("do nothing")
+//            return
+//        }
     }
 }
 
 extension MarketDemand {
-
-    /* New Existing Orders for marketable locomotives are determined
-     It consists of four steps, each step dedicated to one locomotive type:
-        1. Market for Passenger locomotives (green)
-        2. Market for Fast locomotives (red)
-        3. Market for Freight locomotives (yellow)
-        4. Market for Special locomotives (blue)
+    /* -- 1 Generation exists --
+     If this locomotive type does not have the maximum number of dice,
+     add one die to the Customer Base from the dice pool.
+     Then roll all dice in the Customer Base and place them in the empty
+     Existing Order boxes for that locomotive type.
      */
+    func handleOneGeneration(locos: [Locomotive]) -> [Locomotive] {
+
+        print ("Adding orders...")
+        locos.forEach { (locomotive) in
+            print ("Locomotive: \(locomotive.name), \(locomotive.color), \(locomotive.orders)")
+            let orderBook = OrderBook.init(capacity: locomotive.orderCapacity, orders: locomotive.orders)
+            do {
+                let _ = try orderBook.add(.completedOrder)
+            } catch {
+                print ("error -- \(error as Any)")
+            }
+            guard let orderBookOrders = orderBook.orders else {
+                print ("Can't obtain orderbook proxy orders")
+                return
+            }
+            locomotive.orders = orderBookOrders
+        }
+
+        return locos
+    }
+}
+
+extension MarketDemand {
+    func handleTwoGenerations() {
+
+    }
+}
+
+extension MarketDemand {
+    func handleThreeGenerations() {
+
+    }
+}
+
+
+
+/*
+extension MarketDemand {
     internal func marketFor(locomotiveType: Family.Color) -> [Locomotive] {
         guard let locos = self.locos else {
             return [] // empty array
@@ -66,3 +181,4 @@ extension MarketDemand {
     }
 
 }
+*/
