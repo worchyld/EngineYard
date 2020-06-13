3f9db90ebd5e5fc20de432cf68212f10b3451a73
AD <silentflute.ad@gmail.com>
2020-06-13 04:13:41 +0100

    add comment. refactor market demand code

--- Shared/MarketDemand.swift
+++ Shared/MarketDemand.swift
@@ -44,38 +44,56 @@ import Foundation
         e. Repeat this for the newest locomotive
  */
 
-struct GenerationsExist {
-    private var locos: [Locomotive]
+/*
+A. Determine how many generations exist for each locomotive color
+   - A generation exists if it has dice (orders) in either existing or customer base
+*/
+class Market {
+    private let locos: [Locomotive]
 
-    struct Market {
-        var red: [Locomotive]?
-        var green: [Locomotive]?
-        var blue: [Locomotive]?
-        var yellow: [Locomotive]?
-    }
+    lazy var currentMarket: [Locomotive]? = {
+        return findMarket()
+    }()
+    lazy var green: [Locomotive]? = {
+        return currentMarket?.filter({ $0.color == .green })
+    }()
+    lazy var blue: [Locomotive]? = {
+        return currentMarket?.filter({ $0.color == .blue })
+    }()
+    lazy var yellow: [Locomotive]? = {
+        return currentMarket?.filter({ $0.color == .yellow })
+    }()
+    lazy var red: [Locomotive]? = {
+        return currentMarket?.filter({ $0.color == .red })
+    }()
+
 
     init(with locomotives: [Locomotive]) {
         self.locos = locomotives
+        //self.currentMarket = findMarket()
     }
 
-    func currentMarket() -> Market {
-        var market = Market()
-        market.green = self.market(for: .green)
-        market.red = self.market(for: .red)
-        market.yellow = self.market(for: .yellow)
-        market.blue = self.market(for: .blue)
-        return market
+    func demand(for color: Family.Color) -> [Locomotive]? {
+        var filter: [Locomotive]?
+
+        switch color {
+        case .green:
+            filter = self.green
+        case .red:
+            filter = self.red
+        case .yellow:
+            filter = self.yellow
+        case .blue:
+            filter = self.blue
+        }
+
+        return filter
     }
 
-    internal func findMarket(for color: Family.Color) -> [Locomotive]? {
-        return self.market(for: color)
-    }
 
-    // A generation only exists if it has dice (orders) in
-    // either `existingOrders` or `customerBase` (sales),
-    // `initialOrders` should be `rejected`
+    // Not used but kept for now
+    /*
     private func market(for color: Family.Color) -> [Locomotive]? {
-
         // Find all Locomotives that match the type & have orders,
         // sorted by cost & generation ascending
 
@@ -96,20 +114,127 @@ struct GenerationsExist {
         }
         .sorted { (a: Locomotive, b: Locomotive) -> Bool in
             return (
+                (a.color.rawValue < b.color.rawValue) &&
                 (a.cost < b.cost) &&
                 (a.generation.rawValue < b.generation.rawValue)
             )
         }
 
         return filter
+    }*/
+
+    // : Get the current market of locomotives
+    // A generation exists if it has dice (orders) in either
+    //   `existingOrder` or `completeOrder`.  `initialOrder` should be ignored
+    // ---
+    // filtering where: orderState is existingOrders or completeOrders
+    //       and orders is not empty
+    //      sorted by cost, generation, color raw values ascending
+    //
+    internal func findMarket() -> [Locomotive] {
+        print ("Attemping filter: findMarket()")
+
+        let filter = locos
+            .filter {
+                    (!$0.orders.isEmpty) &&
+                ($0.orders.contains(where:
+                        { order in
+                            (
+                                (order.state == .existingOrder) ||
+                                (order.state == .completedOrder)
+                            )
+                        })
+                )
+        }
+        .sorted { (a: Locomotive, b: Locomotive) -> Bool in
+            return (
+                (a.color.rawValue < b.color.rawValue) &&
+                (a.cost < b.cost) &&
+                (a.generation.rawValue < b.generation.rawValue)
+            )
+        }
+
+        print ("Filter found: \(filter.count) results")
+
+        return filter
     }
 }
 
+
 class MarketDemand {
-    private var board: Board
+    private var board: Board?
+    private var market: Market?
 
     init(board: Board) {
+        guard let board = board else {
+            print ("No board exists")
+            return
+        }
         self.board = board
+        self.market = Market.init(with: board)
+    }
+
+    func demand(for color: Family.Color) {
+        guard let market = self.market else {
+            print ("No market object exists")
+            return
+        }
+        guard let demand = market.demand(for: color) else {
+            print ("demand returns nil")
+            return
+        }
+
+        let howMany = howManyGenerationsExist(for: demand)
+
+        // : HANDLE GENERATIONS
+        // : There are 4 cases of existing generations;
+        // --------------------------------------------
+        // (A) No generations exists
+        //      - Nothing is done for this type
+        //
+        // (B) 1 generations exist
+        //      - Check capacity, add 1 die to salespool
+        //      - Re-roll all dice in sales pool and move back to `existingOrders`
+        //
+        // (C) 2 generations exist
+        //      - Older generation: Remove 1 die
+        //      - Older generation: Reroll dice in sales, move back to orders
+        //      - Older generation: Mark as `old`
+        //      - Newer generation: Where possible; add 1 die to the salespool
+        //      - Newer generation: Re-roll all salespool dice and move back to `existingOrders`
+        //
+        // (D) 3 generations exist
+        //      - Oldest generation: Remove all dice from oldest generation
+        //      - Oldest generation: Marked as `obsolete`
+        //      - Middle generation: Where possible, add 1 die to sales pool
+        //      - Middle generation: Re-roll all salespool dice and move back to `existingOrders`
+        //      - Newest generation: Repeat the above 2 steps
+        //      - Middle/Newest state are unchanged
+
+        switch howMany {
+        case 1:
+            let results = handleOneGeneration(locos: demand)
+            print ("\n\nRESULTS -- \(results)")
+
+        case 2:
+            handleTwoGenerations()
+
+        case 3:
+            handleThreeGenerations()
+
+        default:
+            print ("Do nothing")
+        }
+
+    }
+
+    private func howManyGenerationsExist(for market: [Locomotive]) -> Int {
+        switch market.count {
+        case 1...3:
+            return market.count
+        default:
+            return 0
+        }
     }
 }
 
@@ -134,8 +259,8 @@ extension MarketDemand {
             guard let orderBookOrders = orderBook.orders else {
                 print ("Can't obtain orderbook proxy orders")
                 return
-            }
-            locomotive.orders = orderBookOrders
+            }            
+            locomotive.setOrders(orders: orderBookOrders)
         }
 
         return locos
