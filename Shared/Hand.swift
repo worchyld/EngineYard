93a7943330efffe19e3484f22f30ec3169856b1d
AD <silentflute.ad@gmail.com>
2020-06-13 22:08:06 +0100

    adding code for a players hand

--- Shared/Hand.swift
+++ Shared/Hand.swift
@@ -8,4 +8,64 @@
 
 import Foundation
 
-class Hand { }
+enum HandError : Error {
+    case handIsEmpty
+    case alreadyHaveThisCard(_ card: Card)
+    case notAvailable(_ card: Card)
+}
+
+class Hand {
+    private (set) var cards: [Card]?
+
+    init(_ cards: [Card]? = nil) {
+        self.cards = cards
+    }
+}
+
+extension Hand {
+    func add(_ card: Card) throws {
+        if (self.cards == nil) {
+            self.cards = [Card]()
+        }
+
+        do {
+            let ok = try canAdd(card)
+            if ok {
+                self.push(card)
+            }
+        } catch {
+            throw error
+        }
+    }
+
+    internal func canAdd(_ card: Card) throws -> Bool {
+        return true
+    }
+
+
+    private func push(_ card: Card) {
+        self.cards?.append(card)
+    }
+
+}
+
+extension Hand {
+    func remove(_ card: Card, at index: Int) throws {
+        do {
+            let ok = try canRemove(card, at: index)
+            if ok {
+                self.push(card)
+            }
+        } catch {
+            throw error
+        }
+    }
+
+    internal func canRemove(_ card: Card, at index: Int) throws -> Bool {
+        return true
+    }
+
+    private func pop(_ card: Card, at index: Int) {
+
+    }
+}
