14f153006d766c2e04659e072b77445bd2464b5f
AD <silentflute.ad@gmail.com>
2020-06-13 22:08:16 +0100

    adding tests

---
+++ EngineYardTests/HandTests.swift
@@ -0,0 +1,21 @@
+//
+//  HandTests.swift
+//  EngineYardTests
+//
+//  Created by Amarjit on 13/06/2020.
+//  Copyright © 2020 Amarjit. All rights reserved.
+//
+
+import XCTest
+
+@testable import EngineYard
+
+class HandTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+
+
+}
