0307d76f8a76b18f08455311f6a4362fd0540a92
AD <silentflute.ad@gmail.com>
2020-06-21 15:58:31 +0100

    add comment

--- Shared/Wallet.swift
+++ Shared/Wallet.swift
@@ -8,6 +8,8 @@
 
 import Foundation
 
+/** Wallet is an interface/interactor for a player's cash **/
+
 protocol WalletUseCases: SpendingUseCase, ValidateSpendingUseCase {
     var cash: Int { get }
 }
