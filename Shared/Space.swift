4b954f642930ef305d7e7797ea56a93ec73dcb06
AD <silentflute.ad@gmail.com>
2020-06-21 21:49:56 +0100

    add a board space class  (currently empty)

---
+++ Shared/Space.swift
@@ -0,0 +1,16 @@
+//
+//  Space.swift
+//  EngineYard
+//
+//  Created by Amarjit on 21/06/2020.
+//  Copyright © 2020 Amarjit. All rights reserved.
+//
+
+import Foundation
+
+// The board is a linear collection of spaces.
+// Each space holds a `LocomotiveFactory` (AKA: Factory)
+// Each `factory` holds multiple `Locomotive` (AKA Card)
+
+class Space {
+}
