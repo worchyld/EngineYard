70f6b2e916e5a67a9ceef8cf8031f25001fc9371
AD <silentflute.ad@gmail.com>
2020-06-21 16:55:39 +0100

    wallet test addition

---
+++ EngineYardTests/WalletTests.swift
@@ -0,0 +1,27 @@
+//
+//  WalletTests.swift
+//  EngineYardTests
+//
+//  Created by Amarjit on 21/06/2020.
+//  Copyright © 2020 Amarjit. All rights reserved.
+//
+
+import XCTest
+
+@testable import EngineYard
+
+class WalletTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testWallet_SpendZero_Fails() {
+        
+    }
+
+}
