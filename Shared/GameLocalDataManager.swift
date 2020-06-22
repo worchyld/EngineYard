2396211617a72a9cf0aba316aa459d60cd98affe
AD <silentflute.ad@gmail.com>
2020-06-22 16:10:00 +0100

    add empty GameLocalDataManager

---
+++ Shared/GameLocalDataManager.swift
@@ -0,0 +1,17 @@
+//
+//  GameLocalDataManager.swift
+//  EngineYard
+//
+//  Created by Amarjit on 22/06/2020.
+//  Copyright © 2020 Amarjit. All rights reserved.
+//
+
+import Foundation
+
+protocol GameLocalDataManagerInputProtocol: class {
+    //func fetchContent(amount: Int) throws -> [Content]
+}
+
+class GameLocalDataManager: GameLocalDataManagerInputProtocol {
+
+}
