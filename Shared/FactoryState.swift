1b200e3d580d892835ed771e2356ed8984c42f80
AD <silentflute.ad@gmail.com>
2020-06-22 15:15:02 +0100

    adding factorystate

---
+++ Shared/FactoryState.swift
@@ -0,0 +1,13 @@
+//
+//  FactoryState.swift
+//  EngineYard
+//
+//  Created by Amarjit on 22/06/2020.
+//  Copyright © 2020 Amarjit. All rights reserved.
+//
+
+import Foundation
+
+enum FactoryState : Int, Codable, CaseIterable {
+    case notavailable = 0, available
+}
