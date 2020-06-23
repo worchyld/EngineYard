c068f58d1671a417340ae6e6c7d22acb6368818c
AD <silentflute.ad@gmail.com>
2020-06-23 14:33:47 +0100

    overly complicated wallet refactor
    Create an interactor that doesn't know about or care about the model; pass in the thing that conforms to the protocol
    A class credits and debits against the wallet struct
    
    A credit handler just credits against wallet
    A debit handler just debits against wallet
    
    This is an experiment to try to understand single responsibility, but I think its so complex I probably won't use it.

--- Shared/WalletInteractor.swift
+++ Shared/WalletInteractor.swift
@@ -8,36 +8,164 @@
 
 import Foundation
 
-class WalletInteractor {
-    private var wallet: Wallet
-    private var creditDelegate: CreditDelegate?
-    private var debitDelegate: SpendingDelegate?
+protocol WalletUseCase {
+    var wallet: Wallet { get }
+    var balance: Int { get }
+    var creditDelegate: CreditUseCase? { get }
+    var debitDelegate: DebitUseCase?  { get }
+}
+
+protocol CreditUseCase {
+    func credit(amount: Int) throws
+}
+protocol DebitUseCase {
+    func debit(amount: Int) throws
+}
+
+protocol WalletCreditUseCase {
+    func credit(walletHandler: WalletUseCase, amount: Int) throws
+}
+protocol WalletDebitUseCase {
+    func debit(walletHandler: WalletUseCase, amount: Int) throws
+}
+
+/*
+ * Protocol that defines the Interactor's use case.
+ */
+protocol WalletInteractorInput: class, CreditUseCase, DebitUseCase {
+    var walletHandler: WalletUseCase? { get }
+    var creditHandler: WalletCreditUseCase? { get }
+    var debitHandler: WalletDebitUseCase? { get }
+
+    var balance: Int { get }
+}
+
+class WalletHandler : WalletUseCase {
+    private(set) internal var wallet: Wallet
+    private(set) var creditDelegate: CreditUseCase?
+    private(set) var debitDelegate: DebitUseCase?
+
+    var balance: Int {
+        return self.wallet.cash
+    }
 
     init(wallet: Wallet) {
         self.wallet = wallet
+        self.creditDelegate = self
+        self.debitDelegate = self
+    }
+}
+
+extension WalletHandler : CreditUseCase {
+    func credit(amount: Int) {
+        self.wallet.cash += amount
+    }
+}
+
+extension WalletHandler : DebitUseCase {
+    func debit(amount: Int) {
+        self.wallet.cash -= amount
+    }
+}
+
+/*
+ * The Interactor responsible for implementing
+ * the business logic of the module.
+ */
+class WalletInteractor : WalletInteractorInput {
+    internal var walletHandler: WalletUseCase?
+    internal var creditHandler: WalletCreditUseCase?
+    internal var debitHandler: WalletDebitUseCase?
+
+    var balance: Int {
+        return walletHandler?.balance ?? 0
+    }
+
+    init(walletHandler: WalletUseCase) {
+        self.walletHandler = walletHandler
+        self.creditHandler = CreditUseCaseHandler()
+        self.debitHandler = DebitUseCaseHandler()
     }
 
     func credit(amount: Int) throws {
-        creditDelegate = Creditor(self.wallet.cash)
-
-        guard let amount = try creditDelegate?.credit(amount: amount) else {
+        guard let handler = self.walletHandler else {
+            return
+        }
+        guard let creditHandler = creditHandler else {
             return
         }
 
-        updateWallet(amount: amount)
+        do {
+            try creditHandler.credit(walletHandler: handler, amount: amount)
+        } catch {
+            throw error
+        }
     }
 
     func debit(amount: Int) throws {
-        self.debitDelegate = Spender(self.wallet.cash)
-
-        guard let amount = try debitDelegate?.spend(amount: amount) else {
+        guard let handler = self.walletHandler else {
             return
         }
+        guard let debitHandler = debitHandler else {
+            return
+        }
+        do {
+            try debitHandler.debit(walletHandler: handler, amount: amount)
+        }
+        catch {
+            throw error
+        }
 
-        updateWallet(amount: amount)
+    }
+}
+
+
+
+
+
+// Credit Handler
+class CreditUseCaseHandler : WalletCreditUseCase {
+    func credit(walletHandler: WalletUseCase, amount: Int) throws {
+        do {
+            if try canCredit(walletHandler: walletHandler, amount: amount) {
+                try walletHandler.creditDelegate?.credit(amount: amount)
+            }
+        }
+        catch {
+            throw error
+        }
     }
 
-    private func updateWallet(amount: Int) {
-        self.wallet.cash = amount
+    
+    func canCredit(walletHandler: WalletUseCase, amount: Int) throws -> Bool {
+        guard amount.isPositive else {
+            throw SpendingError.mustBePositive(amount)
+        }
+        return true
+    }
+}
+
+// Debit Handler
+class DebitUseCaseHandler : WalletDebitUseCase {
+    func debit(walletHandler: WalletUseCase, amount: Int) throws {
+        do {
+            if try canDebit(walletHandler: walletHandler, amount: amount) {
+                try walletHandler.debitDelegate?.debit(amount: amount)
+            }
+        }
+        catch {
+            throw error
+        }
+    }
+
+    func canDebit(walletHandler: WalletUseCase, amount: Int) throws -> Bool {
+        guard amount.isPositive else {
+            throw SpendingError.mustBePositive(amount)
+        }
+        let balance = walletHandler.balance
+        guard (balance - amount).isPositive else {
+            throw SpendingError.cannotSpend(amount)
+        }
+        return true
     }
 }
