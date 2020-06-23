c6f6c800db728d0d2ac2c072d528ec8d32343854
AD <silentflute.ad@gmail.com>
2020-06-23 07:27:33 +0100

    remove files

--- Shared/GameLocalDataAPI.swift
+++ Shared/GameLocalDataAPI.swift
@@ -9,107 +9,24 @@
 import Foundation
 
 protocol GameLocalDataAPIInputProtocol: class {
-    func fetchFixtures() throws
+    func fetchFixtures(from bundle: Bundle) throws
 }
 protocol HandleResponseDelegate: class {
     func handleResponse(result: Result<Response, Error> )
 }
 
-class GameLocalDataAPI: GameLocalDataAPIInputProtocol, HandleResponseDelegate {
+class GameLocalDataAPI: GameLocalDataAPIInputProtocol {
 
-    private weak var delegate: HandleResponseDelegate!
-
-    struct Config {
-       var bundle: Bundle
-       var resource: String
-       var ofType: String?
-    }
-    var config: Config?
-
-    init(config: Config) {
-        self.delegate = self
-        self.config = config
-    }
-
-    func fetchFixtures() throws {
-        guard let config = config else {
-            throw NSError(domain: "Config missing", code: 0, userInfo: nil)
-        }
-
-        GameLocalDataAPI.debugInfo(bundle: config.bundle)
-
-        if let url = config.bundle.url(forResource: config.resource, withExtension: config.ofType) {
-
-            do {
-                let data = try Data(contentsOf: url, options: .mappedIfSafe)
-
-                print(data as Any)
-
-                decodeJSONResponse(from: data
-                    , completionHandler: { result in
-
-                    self.delegate.handleResponse(result: result)
-                })
-
-            } catch {
-                throw error
-            }
-
-        }
-        else {
-            throw FileError.couldNotLoadFile(config.resource)
-        }
-
-    }
-
-    func handleResponse(result: Result<Response, Error> ) {
-        switch result {
-        case .success(let response):
-            print ("Response -->> \(response)")
-
-        case .failure(let error):
-            print ("Error -->> \(error.localizedDescription) ")
-        }
-    }
-
-    // not used yet
-    private func useFileProvider(url: URL) {
-        let fileProvider = LocalFileProvider()
-
-        fileProvider.requestLocalFile(from: url) { (result) in
-            switch result {
-            case .success(let data):
-                print ("Response -->> \(data)")
-
-                self.decodeJSONResponse(from: data
-                                   , completionHandler: { result in
-
-                                   self.delegate.handleResponse(result: result)
-                })
-
-
-            case .failure(let error):
-                print ("Error -->> \(error.localizedDescription) ")
-
-            }
-        }
-    }
-
-    private func decodeJSONResponse(from data: Data, completionHandler: @escaping((Result<Response, Error>)) -> ()) {
+    func fetchFixtures(from bundle: Bundle) throws {
+        GameLocalDataAPI.debugInfo(bundle: bundle)
 
         do {
-            let decoder = JSONDecoder()
-            decoder.keyDecodingStrategy = .convertFromSnakeCase
-            decoder.dateDecodingStrategy = .secondsSince1970
-
-            let result = try decoder.decode(Response.self, from: data)
-
-            completionHandler(.success(result))
+            let result = try bundle.decode(Response.self, from: "board.json", dateDecodingStrategy: .deferredToDate, keyDecodingStrategy: .convertFromSnakeCase)
+            print (result)
         }
         catch {
-            completionHandler(.failure(error))
+            throw error
         }
-
     }
 
     private static func debugInfo(bundle: Bundle) {
