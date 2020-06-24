b2bd97016c49ed2072c37d0d5094b804d9ca2fe4
AD <silentflute.ad@gmail.com>
2020-06-24 20:14:02 +0100

    adding ordinal extension

--- Shared/Generation.swift
+++ Shared/Generation.swift
@@ -11,3 +11,14 @@ import Foundation
 enum Generation: Int, Codable, CaseIterable {
     case first = 1, second, third, fourth, fifth
 }
+
+
+
+// Ordinal description extension
+extension Generation {
+    var ordinalFormat: String? {
+        let number: NSNumber = NSNumber(integerLiteral: self.rawValue)
+        let cache = NumberFormatCache.ordinalFormat
+        return cache.string(from: number)
+    }
+}
