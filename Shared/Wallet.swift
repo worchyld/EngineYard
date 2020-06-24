edb183a7edf3276b136bef6c6b0bbf655610abc9
AD <silentflute.ad@gmail.com>
2020-06-24 03:12:41 +0100

    Remove/refactor this file  -- going to keep the wallet simple!

--- Shared/Wallet.swift
+++ Shared/Wallet.swift
@@ -1,17 +1,88 @@
 //
-//  Wallet.swift
+//  WalletHandler.swift
 //  EngineYard
 //
-//  Created by Amarjit on 14/06/2020.
+//  Created by Amarjit on 21/06/2020.
 //  Copyright © 2020 Amarjit. All rights reserved.
 //
 
 import Foundation
 
-struct Wallet {
-    var cash: Int
+protocol WalletHolderDelegate: class {
+    var cash: Int { get set }
+}
 
-    init(_ cash: Int = 0) {
-        self.cash = cash
+protocol BankDebitUseCase {
+    func debit(account: WalletHolderDelegate, amount: Int) throws
+}
+protocol BankCreditUseCase {
+    func credit(account: WalletHolderDelegate, amount: Int) throws
+}
+protocol Bank : BankCreditUseCase, BankDebitUseCase {}
+
+
+struct Wallet : Bank {
+
+    func credit(account: WalletHolderDelegate, amount: Int = 0) throws {
+
+        do {
+            if try canAdd(amount: amount) {
+                willAdd(funds: amount, to: account)
+            }
+        }
+        catch {
+            throw error
+        }
+    }
+
+    func debit(account: WalletHolderDelegate, amount: Int = 0) throws {
+
+        let balance = account.cash
+        do {
+            if try canDeduct(balance: balance, bySpending: amount) {
+                willDeduct(account: account, amount: amount)
+            }
+        }
+        catch {
+            throw error
+        }
+    }
+
+
+
+    // MARK: (Private) functions
+
+    private func willAdd(funds: Int, to account: WalletHolderDelegate) {
+        account.cash += funds
+    }
+
+    private func willDeduct(account: WalletHolderDelegate, amount: Int) {
+        account.cash -= amount
     }
 }
+
+extension Wallet {
+    // can deduct balance by spending amount
+    func canDeduct(balance: Int, bySpending amount: Int) throws -> Bool {
+        guard balance.isPositive else {
+            throw SpendingError.notEnoughFunds(balance)
+        }
+        guard amount.isPositive else {
+            throw SpendingError.mustBePositive(amount)
+        }
+        guard ((balance - amount) >= 0) else {
+            throw SpendingError.cannotSpend(amount)
+        }
+        return true
+    }
+
+
+    // Can add cash?
+    func canAdd(amount: Int) throws -> Bool {
+        guard amount.isPositive else {
+           throw SpendingError.mustBePositive(amount)
+        }
+        return true
+    }
+}
+
