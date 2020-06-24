1ec5404a55c0a06d343fdbbfd70d94c6bfc2f02d
AD <silentflute.ad@gmail.com>
2020-06-24 22:18:23 +0100

    adding order type enum

---
+++ Shared/OrderInteractor.swift
@@ -0,0 +1,13 @@
+//
+//  OrderInteractor.swift
+//  EngineYard
+//
+//  Created by Amarjit on 24/06/2020.
+//  Copyright © 2020 Amarjit. All rights reserved.
+//
+
+import Foundation
+
+public enum OrderType : Int {
+    case initialOrder, existingOrder, completedOrder
+}
