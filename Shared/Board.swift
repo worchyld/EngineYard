76b98865f2333d5a02a1d316eba4839ec769258d
AD <silentflute.ad@gmail.com>
2020-06-24 03:11:58 +0100

    clean up the traingame start function

--- Shared/Board.swift
+++ Shared/Board.swift
@@ -14,25 +14,21 @@ class TrainGame {
     var board: Board?
 
     func start() throws {
-        do {
-            if let board = try TrainGame.loadInitialBoard() {
-                self.board = board
-            }
-            else {
-                throw NSError(domain: "Board could not be initialised", code: 0, userInfo: nil)
-            }
+        let board = TrainGame.loadInitialBoard()
 
-        } catch {
-            throw error
-        }
+        print ("board: \(board as Any)")
     }
 }
 
 extension TrainGame {
-    static func loadInitialBoard() throws -> Board? {
+    static func loadInitialBoard() -> Board? {
         let board = [Space?](repeating: nil, count: 14)
 
+        // 1. get response object from data in local json file
 
+        // 2. prepare/build the board
+
+        // 3. save to cloudkit/database?
 
         return board
     }
