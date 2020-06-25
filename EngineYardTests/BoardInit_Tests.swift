e76c821a0488676678bc3225e1a6521bc214d3fa
AD <silentflute.ad@gmail.com>
2020-06-25 23:39:08 +0100

    adding a function to try out the idea of using meta data to validate the decks and cards setup; also a filter on references to find matches in my cards

--- EngineYardTests/BoardInit_Tests.swift
+++ EngineYardTests/BoardInit_Tests.swift
@@ -17,4 +17,89 @@ class BoardInit_Tests: XCTestCase {
     }
 
 
+    func testBoardInit() throws {
+        let bundle = Bundle(for: type(of: self))
+        let file = Constants.boardJSONFile
+        let api = DataLoader.shared
+        let result = api.load(from: bundle, file: file)
+
+        switch result {
+        case .success(let response):
+
+            guard let meta = response.meta else {
+                XCTFail("No meta found")
+                return
+            }
+
+            guard let factories = response.factories else {
+                XCTFail("No factories found")
+                return
+            }
+            guard let cards = response.cards else {
+                XCTFail("No cards found")
+                return
+            }
+
+            for f in factories {
+                XCTAssertNil(f.cards)
+            }
+
+            for f in factories {
+
+                var f = f
+                XCTAssertEqual(f.cost % 4, 0)
+                XCTAssertNotNil(f.references)
+                let livery = f.livery
+
+                print ("\n\nEvaluating: \(f.name), \(f.livery), \(f.generation)\n")
+
+                XCTAssertNotNil(f.references)
+                XCTAssertTrue(f.generation.rawValue > 0 && f.generation.rawValue <= 5)
+
+                guard let references = f.references else {
+                    XCTFail("No references found")
+                    return
+                }
+
+                let generation = (f.generation.rawValue - 1)
+
+                for (index, ref) in references.enumerated() {
+
+                    print ("\nINDEX -- \(index), REF: \(ref.id)")
+
+
+                    let metaGenerations: Int = {
+                        switch livery {
+                        case .green:
+                            return meta.cards.green.generations[generation]
+                        case .red:
+                            return meta.cards.red.generations[generation]
+                        case .yellow:
+                            return meta.cards.yellow.generations[generation]
+                        case .blue:
+                            return meta.cards.blue.generations[generation]
+                        }
+                    }()
+
+                    print ("\nMeta found: \(metaGenerations) results for \(f.livery) \(f.generation)")
+
+                    XCTAssertEqual(references.count, metaGenerations, "ref: \(references.count) vs \(metaGenerations) -- (Failed on :\(f.livery), \(f.generation), Ref: \(ref.id)")
+
+                    let foundList = cards.filter { r in references.contains(where: { $0.id == r.id }) }
+
+                    XCTAssertEqual(foundList.count, metaGenerations, "(Failed on :\(f.livery), \(f.generation), Found: \(foundList.count) vs \(metaGenerations)")
+                }
+
+            }
+
+            for f in factories {
+                print (f.name, f.cards as Any)
+            }
+
+
+        case .failure(let error):
+           throw(error)
+        }
+    }
+
 }
