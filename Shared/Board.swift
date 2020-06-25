34b2983344d84db73d5c0cecdf3bce7df2d8f275
AD <silentflute.ad@gmail.com>
2020-06-25 14:00:56 +0100

    adding a dataloader; removed completion blocks for now

--- Shared/Board.swift
+++ Shared/Board.swift
@@ -30,8 +30,60 @@ extension TrainGame {
         var board = [Factory?](repeating: nil, count: 14)
 
 
+
         return board
     }
 
 }
 
+//typealias ResponseHandler = (Result<Response, Error>) -> Void
+
+class DataLoader {
+    static let shared = DataLoader()
+
+    private init() { }
+
+    public func load(from bundle: Bundle = Bundle.main, _ file: String = Constants.boardJSONFile) -> Result<Response, Error>? {
+        do {
+            let result = try bundle.decode(Response.self, from: file, dateDecodingStrategy: .deferredToDate, keyDecodingStrategy: .convertFromSnakeCase)
+
+            return Result.success(result)
+        }
+        catch {
+            return Result.failure(error)
+        }
+    }
+
+    public func save(rootObject: Response) -> Result<Bool, Error> {
+        let result = self.encode(rootObject: rootObject)
+
+        switch result {
+
+        case .success(let data):
+            print ("RECIEVED DATA >>> \(data)")
+
+            if let jsonString = String(data: data, encoding: .utf8) {
+                print ("JSON STRING >> \(jsonString)")
+                #warning("save not implemented yet")
+            }
+
+            return Result.success(true)
+
+        case .failure(let error):
+            return Result.failure(error)
+
+        }
+    }
+
+    internal func encode(rootObject: Response) -> Result<Data, Error> {
+        let encoder = JSONEncoder()
+        do {
+            let gameData = try encoder.encode( rootObject.self )
+            return Result.success(gameData)
+        }
+        catch {
+            return Result.failure(error)
+        }
+    }
+
+}
