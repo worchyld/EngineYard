55c2a8b9a69d25aaa247187f2f7d2921abcc0c11
AD <silentflute.ad@gmail.com>
2020-06-25 13:28:41 +0100

    remove the fixtures loader -- i think we just need to make it much much easier!

--- EngineYardTests/Fixtures.swift
+++ EngineYardTests/Fixtures.swift
@@ -11,19 +11,19 @@ import  XCTest
 
 struct Fixtures {
     static var locomotives: [Locomotive] {
-        let greenFirst = Locomotive(id: "green.1", ref: "green.1", name: "green.1", avatar: "green-1.png", cost: 4, generation: .first, livery: .green)
-        let redFirst = Locomotive(id: "red.1", ref: "red.1", name: "red.1", avatar: "red-1.png", cost: 8, generation: .first, livery: .red)
-        let yellowFirst = Locomotive(id: "yellow.1", ref: "yellow.1", name: "yellow.1", avatar: "yellow-1.png", cost: 12, generation: .first, livery: .yellow)
-        let blueFirst = Locomotive(id: "blue.1", ref: "blue.1", name: "blue.1", avatar: "blue-1.png", cost: 18, generation: .first, livery: .blue)
+        let greenFirst = Locomotive(id: UUID(), ref: "green.1", name: "green.1", avatar: "green-1.png", cost: 4, generation: .first, livery: .green)
+        let redFirst = Locomotive(id: UUID(), ref: "red.1", name: "red.1", avatar: "red-1.png", cost: 8, generation: .first, livery: .red)
+        let yellowFirst = Locomotive(id: UUID(), ref: "yellow.1", name: "yellow.1", avatar: "yellow-1.png", cost: 12, generation: .first, livery: .yellow)
+        let blueFirst = Locomotive(id: UUID(), ref: "blue.1", name: "blue.1", avatar: "blue-1.png", cost: 18, generation: .first, livery: .blue)
 
         return [greenFirst, redFirst, yellowFirst, blueFirst]
     }
 
     static var factories: [Factory] {
-        let greenFirst = Factory(id: "green.1", ref: "green.1", avatar: "green.1", cost: 4, initialOrder: 0, existingOrders: [], completedOrders: [], cards: [], available: false, livery: .green, generation: .first, rusting: Rusting.notRusting, orderCapacity: 3)
-        let redFirst = Factory(id: "green.1", ref: "green.1", avatar: "green.1", cost: 4, initialOrder: 0, existingOrders: [], completedOrders: [], cards: [], available: false, livery: .green, generation: .first, rusting: Rusting.notRusting, orderCapacity: 3)
-        let yellowFirst = Factory(id: "yellow.1", ref: "yellow.1", avatar: "yellow.1", cost: 12, initialOrder: 0, existingOrders: [], completedOrders: [], cards: [], available: false, livery: .blue, generation: .first, rusting: Rusting.notRusting, orderCapacity: 3)
-        let blueFirst = Factory(id: "blue.1", ref: "blue.1", avatar: "blue.1", cost: 18, initialOrder: 0, existingOrders: [], completedOrders: [], cards: [], available: false, livery: .blue, generation: .first, rusting: Rusting.notRusting, orderCapacity: 3)
+        let greenFirst = Factory(id: UUID(), ref: "green.1", avatar: "green.1", cost: 4, initialOrder: 0, existingOrders: [], completedOrders: [], cards: [], available: false, livery: .green, generation: .first, rusting: Rusting.notRusting, orderCapacity: 3)
+        let redFirst = Factory(id: UUID(), ref: "green.1", avatar: "green.1", cost: 4, initialOrder: 0, existingOrders: [], completedOrders: [], cards: [], available: false, livery: .green, generation: .first, rusting: Rusting.notRusting, orderCapacity: 3)
+        let yellowFirst = Factory(id: UUID(), ref: "yellow.1", avatar: "yellow.1", cost: 12, initialOrder: 0, existingOrders: [], completedOrders: [], cards: [], available: false, livery: .blue, generation: .first, rusting: Rusting.notRusting, orderCapacity: 3)
+        let blueFirst = Factory(id: UUID(), ref: "blue.1", avatar: "blue.1", cost: 18, initialOrder: 0, existingOrders: [], completedOrders: [], cards: [], available: false, livery: .blue, generation: .first, rusting: Rusting.notRusting, orderCapacity: 3)
 
         return [greenFirst, redFirst, yellowFirst, blueFirst]
     }
