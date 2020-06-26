ba59a262795f8b38d6a817bb2c1346d3ed26bc93
AD <silentflute.ad@gmail.com>
2020-06-26 13:37:19 +0100

    adding an empty production shifter class;

---
+++ Shared/ProductionShifter.swift
@@ -0,0 +1,38 @@
+//
+//  ProductionShifter.swift
+//  EngineYard
+//
+//  Created by Amarjit on 26/06/2020.
+//  Copyright © 2020 Amarjit. All rights reserved.
+//
+
+import Foundation
+
+protocol ProductionShifterUseCase {
+    func shift(production: Int, from: Card, to: Card) throws
+}
+
+class ProductionShifter : ProductionShifterUseCase {
+
+    init() {}
+
+    func shift(production: Int, from: Card, to: Card) throws {
+
+    }
+}
+
+
+extension ProductionShifter {
+    func canShift(production: Int, from: Card, to: Card) throws -> Bool {
+        return true
+    }
+
+    func canAffordShifting(production: Int, from: Card, to: Card) -> Bool {
+        return true
+    }
+
+    func costOfShifting(production: Int, from: Card, to: Card) -> Int {
+        return 0
+    }
+
+}
