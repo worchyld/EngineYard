96c21af4e2d8dbf781be7974b412f7f84615dbb6
AD <silentflute.ad@gmail.com>
2020-06-26 01:09:01 +0100

    remove meta as a var from response

--- Shared/Response.swift
+++ Shared/Response.swift
@@ -9,12 +9,10 @@
 import Foundation
 
 struct Response: Codable {
-    var meta: Meta?
     var factories: [Factory]?
     var cards: [Card]?
 
     enum CodingKeys: String, CodingKey {
-        case meta = "_meta"
         case factories, cards
     }
 }
