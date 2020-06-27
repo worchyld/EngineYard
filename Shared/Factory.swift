b2d89e4853f170ce3ac420c709bbd32248247473
AD <silentflute.ad@gmail.com>
2020-06-27 11:14:14 +0100

    make the card conform to ProductionInputDelegate so we can run tests against it move production and income into the struct add production/spent production as int vars add custom coding keys with init and encoder

--- Shared/Factory.swift
+++ Shared/Factory.swift
@@ -23,30 +23,79 @@ struct Factory: Codable {
 }
 
 // MARK: - Card
-struct Card: Codable {
+struct Card: Codable, ProductionInputDelegate {
     let id: UUID
     let name, avatar: String
     let cost: Int
     let generation: Generation
     let livery: Livery
-}
+    var production: Int
+    var spentProduction: Int
 
-protocol ProductionAndIncome {
-    var productionCost: Int { get }
-    var income: Int { get }
-}
-
-extension Factory : ProductionAndIncome {
     var productionCost: Int {
-        guard (self.cost % 4 == 0) else { return 0 }
-        return Int( floor(Double(self.cost / 2)) )
-    }
+       guard (self.cost % 4 == 0) else { return 0 }
+       return Int( floor(Double(self.cost / 2)) )
+   }
+
     var income: Int {
         guard (cost % 4 == 0) else { return 0 }
         return Int( floor(Double(productionCost / 2)) )
     }
+
+    init(id: UUID?, name: String, avatar: String, cost: Int, generation: Generation, livery: Livery ) {
+        self.id = id ?? UUID()
+        self.name = name
+        self.avatar = avatar
+        self.cost = cost
+        self.generation = generation
+        self.livery = livery
+        self.production = 0
+        self.spentProduction = 0
+    }
+
+    private enum CodingKeys: String, CodingKey {
+        case id, name, avatar, cost, generation, livery
+        case production, spentProduction
+    }
+
 }
 
+extension Card {
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+
+        id = try container.decode(UUID.self, forKey: .id)
+        name = try container.decode(String.self, forKey: .name)
+        avatar = try container.decode(String.self, forKey: .avatar)
+        let cost = try container.decode(Int.self, forKey: .cost).clamp(low: 4, high: 56)
+        if ((cost % 4) != 0) {
+            throw NSError(domain: "Cost is not a modulus of 4", code: 0, userInfo: nil)
+        }
+        guard (cost >= 4 && cost <= 56 ) else {
+            throw NSError(domain: "Cost is not in the expected range of 4-56", code: 0, userInfo: nil)
+        }
+        self.cost = cost
+
+        livery = try container.decode(Livery.self, forKey: .livery)
+        generation = try container.decode(Generation.self, forKey: .generation)
+        production = try container.decodeIfPresent(Int.self, forKey: .production) ?? 0
+        spentProduction = try container.decodeIfPresent(Int.self, forKey: .spentProduction) ?? 0
+    }
+
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+
+        try container.encode(id, forKey: .id)
+        try container.encode(name, forKey: .name)
+        try container.encode(avatar, forKey: .avatar)
+        try container.encode(cost, forKey: .cost)
+        try container.encode(livery, forKey: .livery)
+        try container.encode(generation, forKey: .generation)
+        try container.encode(production, forKey: .production)
+        try container.encode(spentProduction, forKey: .spentProduction)
+    }
+}
 
 
 extension Factory {
