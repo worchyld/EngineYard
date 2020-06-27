8c246b1146591256835112408d0a66730a728d3b
AD <silentflute.ad@gmail.com>
2020-06-27 17:12:05 +0100

    create BoardCreationTests

---
+++ EngineYardTests/BoardCreationTests.swift
@@ -0,0 +1,21 @@
+//
+//  BoardCreationTests.swift
+//  EngineYardTests
+//
+//  Created by Amarjit on 27/06/2020.
+//  Copyright © 2020 Amarjit. All rights reserved.
+//
+
+import XCTest
+
+class BoardCreationTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+}
