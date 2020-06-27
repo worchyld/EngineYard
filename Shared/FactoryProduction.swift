d94a99c23103424a0a1989bffeef21107080b03a
AD <silentflute.ad@gmail.com>
2020-06-27 20:41:10 +0100

    creating tests against spending production

--- Shared/FactoryProduction.swift
+++ Shared/FactoryProduction.swift
@@ -13,15 +13,25 @@ class FactoryProduction : Identifiable, Codable {
     var units: Int
     var spent: Int
 
-    init(id: UUID, units: Int = 0, spent: Int = 0) {
+    init(id: UUID, units: Int = 0, _ spent: Int = 0) {
         self.id = id
         self.units = units
         self.spent = spent
+    }
+
+    private enum CodingKeys: String, CodingKey {
+        case id, units, spent
     }
 }
 
 extension FactoryProduction : Equatable {
     static func == (lhs: FactoryProduction, rhs: FactoryProduction) -> Bool {
         return (lhs.id == rhs.id)
+    }
+}
+
+extension FactoryProduction : CustomStringConvertible {
+    var description: String {
+        return ("units: \(self.units), spent: \(spent)")
     }
 }
--- Shared/ProductionHandler.swift
+++ Shared/ProductionHandler.swift
@@ -12,18 +12,36 @@ import Foundation
 
 protocol ProductionUseCase {
     func increase()
-    func spend()
+    //func spend()
     func reset()
     func shift()
 }
 
 class ProductionHandler : ProductionUseCase {
+    var fp: FactoryProduction!
+    internal var spendingDelegate: Spender?
+    internal var increaserDelegate: Increaser?
+
+    init(with fp: FactoryProduction) {
+        self.fp = fp
+        self.spendingDelegate = Spender(fp.units)
+        self.increaserDelegate = Increaser(fp.units)
+    }
+
     func increase() {
 
     }
 
-    func spend() {
-
+    func spend(amount: Int) throws -> Int {
+        do {
+            guard let result = try self.spendingDelegate?.spend(amount: amount) else {
+                throw SpendingError.cannotSpend(amount)
+            }
+            return result
+        }
+        catch {
+            throw error
+        }
     }
 
     func reset() {
