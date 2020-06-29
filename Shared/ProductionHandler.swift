0201cd3baa359eecb7a4f5e2ed7a75f8173076ed
AD <silentflute.ad@gmail.com>
2020-06-29 19:51:13 +0100

    add a ShiftProductionUseCase delegate

--- Shared/ProductionHandler.swift
+++ Shared/ProductionHandler.swift
@@ -10,6 +10,8 @@ import Foundation
 
 class ProductionHandler {
 
+    private var shiftProductionHandler: ShiftProductionUseCase?
+
     /// Reset production to value set in spent; so long as you own the card resetting
     func reset(production: FactoryProduction, owner: Player) throws -> FactoryProduction {
         do {
@@ -32,31 +34,23 @@ class ProductionHandler {
     }
 
 
-
-
-
-    // MARK: - Shift Production methods
-
-    func costToShift(production units: Int, from origin: FactoryProduction, to destination: FactoryProduction) throws -> Int {
-        do {
-            if try canShift(production: units, from: origin, to: destination) {
-                // do something
-                
-            }
-            return 0
-        }
-        catch {
-            throw error
-        }
-    }
+    // MARK: Shift production
 
     /// Shift production from 1 card to another
-    func shift(production units: Int, from origin: FactoryProduction, to destination: FactoryProduction) throws -> FactoryProduction {
+    func shift(production units: Int, from origin: FactoryProduction, to destination: FactoryProduction) throws -> FactoryProduction? {
+
+        self.shiftProductionHandler = ShiftProductionHandler()
+
         do {
-            if try canShift(production: units, from: origin, to: destination) {
-                // do something
+            guard let delegate = shiftProductionHandler else {
+                throw ProductionError(reason: .missingHandler)
             }
-            return origin
+            if let result = try delegate.shift(production: units, from: origin, to: destination) {
+                // do something
+
+                return result
+            }
+            return nil
         }
         catch {
             throw error
@@ -75,26 +69,5 @@ extension ProductionHandler {
             throw CardError(reason: .missingCard)
         }
         return true
-    }
-}
-
-extension ProductionHandler {
-    /// + Must own both the origin and destination card
-    /// + origin card cannot be destination card
-    /// + destination card must be older than the origin card
-    /// + destination must be available (hasOrders == true) && (not rusting)
-    /// + must be able to afford the shift
-    func canShift(production units: Int, from origin: FactoryProduction, to destination: FactoryProduction) throws -> Bool {
-        return true
-    }
-
-    /// Rules state that if post-shifting; if units on origin card are nil; remove card from game
-    func willShiftingProductionLoseCard(_ units: Int, origin: FactoryProduction) -> Bool {
-        return true
-    }
-
-    /// Check if origin card was left empty; if it is; remove the card from game (mark it as deleted)
-    func postShift(origin: FactoryProduction) {
-
     }
 }
