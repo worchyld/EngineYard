652ed307e31aa053389e5a367c97c41ad7960dd6
AD <silentflute.ad@gmail.com>
2020-07-01 20:22:42 +0100

    adding test

---
+++ EngineYardTests/CDTests.swift
@@ -0,0 +1,45 @@
+//
+//  CDTests.swift
+//  EngineYardTests
+//
+//  Created by Amarjit on 01/07/2020.
+//  Copyright © 2020 Amarjit. All rights reserved.
+//
+
+import XCTest
+import CoreData
+
+class CDTests: EngineYardTests {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testDidCreate() {
+
+    }
+
+    func testDidEdit() {
+
+    }
+
+    func testDidRead() {
+
+    }
+
+    func testDidDelete() {
+
+    }
+
+    func testHasRelationships() {
+
+    }
+
+    func testOrderTypes() {
+        
+    }
+}
