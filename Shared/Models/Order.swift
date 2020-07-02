2265aff7814d27ea4a9756ea72e87f5657c882f3
AD <silentflute.ad@gmail.com>
2020-07-02 19:38:37 +0100

    adding order models (structs and enums)

---
+++ Shared/Models/Order.swift
@@ -0,0 +1,13 @@
+//
+//  Order.swift
+//  EngineYard
+//
+//  Created by Amarjit on 02/07/2020.
+//  Copyright © 2020 Amarjit. All rights reserved.
+//
+
+import Foundation
+
+struct Order {
+    let value: Int
+}
---
+++ Shared/Models/OrderType.swift
@@ -0,0 +1,13 @@
+//
+//  OrderType.swift
+//  EngineYard
+//
+//  Created by Amarjit on 02/07/2020.
+//  Copyright © 2020 Amarjit. All rights reserved.
+//
+
+import Foundation
+
+enum OrderType : NSInteger, CaseIterable {
+    case initialOrder, existingOrder, completedOrder
+}
