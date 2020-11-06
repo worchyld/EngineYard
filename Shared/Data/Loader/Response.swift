a3cba364e1990b91801ff29e1b2f77e0f11de1db
worchyld <silentflute.ad@gmai.com>
2020-11-06 17:41:48 +0000

    asset updates

---
+++ Shared/Data/Loader/Response.swift
@@ -0,0 +1,17 @@
+//
+//  Response.swift
+//  EngineYardTests
+//
+//  Created by Amarjit on 22/06/2020.
+//  Copyright © 2020 Amarjit. All rights reserved.
+//
+
+import Foundation
+
+struct Response: Codable {
+    var factories: [Factory]?
+
+    enum CodingKeys: String, CodingKey {
+        case factories
+    }
+}
