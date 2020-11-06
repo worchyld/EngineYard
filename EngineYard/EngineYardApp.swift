b2a0bf687d72be3e9434f496c106bfaf32b0252f
worchyld <silentflute.ad@gmai.com>
2020-11-06 17:36:51 +0000

    move folder

---
+++ EngineYard/EngineYardApp.swift
@@ -0,0 +1,17 @@
+//
+//  EngineYardApp.swift
+//  EngineYard
+//
+//  Created by Amarjit on 06/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct EngineYardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Shared/Helpers/NumberFormatCache.swift
@@ -0,0 +1,34 @@
+//
+//  NumberFormatCache.swift
+//  EngineYard
+//
+//  Created by Amarjit on 02/06/2020.
+//  Copyright © 2020 Amarjit. All rights reserved.
+//
+
+import Foundation
+
+struct NumberFormatCache {
+    static let currencyRateFormatter: NumberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.locale = Locale(identifier: "en_US")
+        formatter.numberStyle = .currency
+        formatter.minimumFractionDigits = 0
+        formatter.maximumFractionDigits = 0
+        formatter.allowsFloats = false
+        formatter.roundingMode = .ceiling
+        formatter.alwaysShowsDecimalSeparator = false
+        return formatter
+    }()
+
+    static let ordinalFormat: NumberFormatter = {
+        let format = NumberFormatter()
+        format.locale = Locale(identifier: "en_US")
+        format.numberStyle = .ordinal
+        format.maximumFractionDigits = 0
+        format.allowsFloats = false
+        format.roundingMode = .ceiling
+        format.alwaysShowsDecimalSeparator = false
+        return format
+    }()
+}
