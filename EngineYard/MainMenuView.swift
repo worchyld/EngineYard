875c73c672f441dbeef57e4402c6132d92c5ecc5
worchyld <silentflute.ad@gmai.com>
2020-11-20 05:55:39 +0000

    Creating a dummy main menu page

---
+++ EngineYard/MainMenuView.swift
@@ -0,0 +1,27 @@
+//
+//  MainMenuView.swift
+//  EngineYard
+//
+//  Created by Amarjit on 20/11/2020.
+//
+
+import SwiftUI
+
+struct MainMenuView: View {
+    var body: some View {
+        HStack(alignment: .center, spacing: 20, content: {
+            Button("Play") {
+                
+            }
+            Button("Leaderboard") {
+                
+            }
+        })
+    }
+}
+
+struct MainMenuView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainMenuView()
+    }
+}
