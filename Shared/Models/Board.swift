196f0ff64569428868e709af1f69b887e3e10ed5
AD <silentflute.ad@gmail.com>
2022-05-29 10:27:45 +0100

    remove un-neccessary files, update json, prepare deck code start

--- Shared/Models/Board.swift
+++ Shared/Models/Board.swift
@@ -2,28 +2,26 @@
 //  Board.swift
 //  EngineYard
 //
-//  Created by Amarjit on 06/11/2020.
+//  Created by Amarjit on 29/05/2022.
 //
 
 import Foundation
 
-class BoardSpace {
-    var available: Bool = false
-    var deck: [Card] = [Card]()
-    
-    init(with deck:[Card]) {
-        self.deck = deck
-    }
-    
-    func toggleAvailability() {
-        self.available = !self.available
-    }
-    
-    func makeNextSpaceAvailable() {
-        // TBD
-    }
+class Board {
 }
 
-class Board {
-    private (set) var spaces: [BoardSpace] = [BoardSpace]()
+extension Board {
+    
+    
+    private func prepare() {
+//        let board = Board()
+//        
+//        let cardsGen1 = [
+//            Card.init(name: "Green.1", image: .greenTrain, cost: 4, livery: .green, generation: .first, trainPool: 4, maxDice: 3),
+//            Card.init(name: "Green.1", image: .greenTrain, cost: 4, livery: .green, generation: .first, trainPool: 4, maxDice: 3),
+//            Card.init(name: "Green.1", image: .greenTrain, cost: 4, livery: .green, generation: .first, trainPool: 4, maxDice: 3),
+//            Card.init(name: "Green.1", image: .greenTrain, cost: 4, livery: .green, generation: .first, trainPool: 4, maxDice: 3)
+//        ]
+        
+    }
 }
--- Shared/Models/Game.swift
+++ Shared/Models/Game.swift
@@ -8,11 +8,6 @@
 import Foundation
 
 class Game {
-    public private (set) var deck: [Card] = [Card]()
     
-    func createDeck() -> [Card] {
-        let deck: [Card] = [Card]()
-                
-        return deck
-    }
+    
 }
