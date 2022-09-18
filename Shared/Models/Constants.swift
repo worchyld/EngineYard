0f98a0e3877247aedc6b364214aade58e7461fa5
AD <silentflute.ad@gmail.com>
2022-09-18 04:43:53 +0100

    Adding constants file

---
+++ Shared/Models/Constants.swift
@@ -0,0 +1,38 @@
+//
+//  Constants.swift
+//  EngineYard
+//
+//  Created by Amarjit on 18/09/2022.
+//
+
+import Foundation
+
+// Constants
+
+public struct Constants {
+    public static let taxRate: Float = 0.10
+    
+    public static let totalDecks = 14
+    public static let totalCards = 43
+    
+    public struct Green {
+        public static let totalCards = 20
+        public static let generations = 5
+        public static let cardsPerGeneration = [4,4,4,4,4]
+    }
+    public struct Red {
+        public static let totalCards = 13
+        public static let generations = 4
+        public static let cardsPerGeneration = [3,3,4,0,0]
+    }
+    public struct Yellow {
+        public static let totalCards = 7
+        public static let generations = 3
+        public static let cardsPerGeneration = [2,2,3,0,0]
+    }
+    public struct Blue {
+        public static let totalCards = 3
+        public static let generations = 2
+        public static let cardsPerGeneration = [1,2]
+    }
+}
