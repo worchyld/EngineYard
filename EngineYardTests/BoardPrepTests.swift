23ac1019e45400c1d819c1d83f9c322fa46c6482
AD <silentflute.ad@gmail.com>
2022-09-18 04:45:55 +0100

    Adding tests for card and deck

---
+++ EngineYardTests/BoardPrepTests.swift
@@ -0,0 +1,41 @@
+//
+//  BoardPrepTests.swift
+//  EngineYardTests
+//
+//  Created by Amarjit on 18/09/2022.
+//
+
+import XCTest
+
+@testable import EngineYard
+
+// Test board preperation
+
+final class BoardPrepTests: XCTestCase {
+
+    var board: Board?
+    
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        self.board = Board()
+        self.board!.decks = self.board!.prepare()
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testDecksCount() throws {
+        XCTAssertTrue(self.board!.decks.count == Constants.totalDecks)
+    }
+    
+    
+    func testDeckPrintOut() throws {
+        for deck in self.board!.decks {
+            for card in deck.cards {
+                print (card.name, card.livery, card.generation, card.cost)
+            }
+        }
+    }
+    
+}
--- EngineYardTests/CardSetupTests.swift
+++ EngineYardTests/CardSetupTests.swift
@@ -12,15 +12,65 @@ import XCTest
 
 class CardSetupTests: XCTestCase {
 
+    var cards: [Card]?
+        
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        let board = Board()
+        self.cards = board.makeCards()
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testInitalSeedOfCards() {
+    func testTotalCards() throws {
+        XCTAssertTrue(cards!.count == Constants.totalCards)
+    }
+    
+    func testTotalGreenCards() throws {
+        let filtered = cards!.filter {
+            $0.livery == .green
+        }.count
+        XCTAssertTrue(filtered == Constants.Green.totalCards)
+    }
+    
+    func testTotalRedCards() throws {
+        let filtered = cards!.filter {
+            $0.livery == .red
+        }.count
+        XCTAssertTrue(filtered == Constants.Red.totalCards)
+    }
+    
+    func testTotalYellowCards() throws {
+        let filtered = cards!.filter {
+            $0.livery == .yellow
+        }.count
+        XCTAssertTrue(filtered == Constants.Yellow.totalCards)
+    }
+    
+    func testTotalBlueCards() throws {
+        let filtered = cards!.filter {
+            $0.livery == .blue
+        }.count
+        XCTAssertEqual(filtered, Constants.Blue.totalCards)
+    }
+    
+    // Testing generation count
+    
+    func testGreenGenerationsCount() throws {
+        
+    }
+    
+    func testRedGenerationsCount() throws {
+        
+    }
+    
+    func testYellowGenerationsCount() throws {
+        
+    }
+    
+    func testBlueGenerationsCount() throws {
         
     }
 }
