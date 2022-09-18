80b8d335c0379f7fc3f8872eb0ee29bace65946d
AD <silentflute.ad@gmail.com>
2022-09-18 04:46:56 +0100

    Refactor the way the board is prepared

--- Shared/Models/Board.swift
+++ Shared/Models/Board.swift
@@ -8,23 +8,115 @@
 import Foundation
 
 class Board {
-    public private (set) var space: [BoardSpace] = [BoardSpace]()
+    var decks : [Deck] = [Deck]()
 }
 
 extension Board {
-    func prepare() {
-//        let board = Board()
-//        
-//        let cardsGen1 = [
-//            Card.init(name: "Green.1", image: .greenTrain, cost: 4, livery: .green, generation: .first, trainPool: 4, maxDice: 3),
-//            Card.init(name: "Green.1", image: .greenTrain, cost: 4, livery: .green, generation: .first, trainPool: 4, maxDice: 3),
-//            Card.init(name: "Green.1", image: .greenTrain, cost: 4, livery: .green, generation: .first, trainPool: 4, maxDice: 3),
-//            Card.init(name: "Green.1", image: .greenTrain, cost: 4, livery: .green, generation: .first, trainPool: 4, maxDice: 3)
-//        ]
-    }
-}
+    func prepare() -> [Deck] {
+        
+        let cards = self.makeCards()
+        
+        let decks : [Deck] = [
+            Deck(4, .green, .first, 1),
+            Deck(8, .red, .first, 1),
+            Deck(12, .yellow, .first, 3),
+            Deck(16, .blue, .first, 3),
+            Deck(20, .green, .second, 3),
+            Deck(24, .red, .second, 3),
+            Deck(28, .yellow, .second, 2),
+            Deck(32, .green, .third, 3),
+            Deck(36, .blue, .second, 3),
+            Deck(40, .red, .third, 3),
+            Deck(44, .green, .fourth, 3),
+            Deck(48, .yellow, .third, 3),
+            Deck(52, .red, .fourth, 3),
+            Deck(56, .green, .fifth, 5),
+        ]
 
-class BoardSpace {
-    var deck: [Card] = [Card]()
-    var active: Bool = false
+        // Fill deck where the cost is the same
+        // it is assumed the colour and generation are fine
+        let _ = decks.map {
+            let deck = $0
+            let cards = cards.filter {
+                $0.cost == deck.cost
+            }
+            deck.cards.append(contentsOf: cards)
+        }
+        
+        return decks
+    }
+    
+    internal func makeCards() -> [Card] {
+        var cards: [Card] = [Card]()
+        
+        
+        for _ in 1...4 {
+            let card = Card(4, .green, .first)
+            cards.append(card)
+        }
+        
+        for _ in 1...4 {
+            let card = Card(20, .green, .second)
+            cards.append(card)
+        }
+        
+        for _ in 1...4 {
+            let card = Card(32, .green, .third)
+            cards.append(card)
+        }
+        
+        for _ in 1...4 {
+            let card = Card(44, .green, .fourth)
+            cards.append(card)
+        }
+        
+        for _ in 1...4 {
+            let card = Card(56, .green, .fifth)
+            cards.append(card)
+        }            
+        
+        for _ in 1...3 {
+            let card = Card(8, .red, .first)
+            cards.append(card)
+        }
+        
+        for _ in 1...3 {
+            let card = Card(24, .red, .second)
+            cards.append(card)
+        }
+        
+        for _ in 1...3 {
+            let card = Card(40, .red, .third)
+            cards.append(card)
+        }
+        
+        for _ in 1...4 {
+            let card = Card(52, .red, .fourth)
+            cards.append(card)
+        }
+        
+        for _ in 1...2 {
+            let card = Card(12, .yellow, .first)
+            cards.append(card)
+        }
+        
+        for _ in 1...2 {
+            let card = Card(28, .yellow, .second)
+            cards.append(card)
+        }
+        
+        for _ in 1...3 {
+            let card = Card(48, .yellow, .third)
+            cards.append(card)
+        }
+        
+        cards.append( Card(16, .blue, .first) )
+        
+        for _ in 1...2 {
+            let card = Card(36, .blue, .second)
+            cards.append(card)
+        }
+
+        return cards
+    }
 }
