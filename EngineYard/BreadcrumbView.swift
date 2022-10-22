59f129059c3345a8d6c4a82efea1733ceb4a7d7d
AD <silentflute.ad@gmail.com>
2022-10-22 06:02:21 +0100

    add view

---
+++ EngineYard/BreadcrumbView.swift
@@ -0,0 +1,22 @@
+//
+//  BreadcrumbView.swift
+//  EngineYard
+//
+//  Created by Amarjit on 22/10/2022.
+//
+
+import SwiftUI
+
+// Simple view to show the user where they are in the game flow
+
+struct BreadcrumbView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BreadcrumbView_Previews: PreviewProvider {
+    static var previews: some View {
+        BreadcrumbView()
+    }
+}
