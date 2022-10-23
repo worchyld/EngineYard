f23ed94d38f8c693598799165de12853d5fc9a1d
AD <silentflute.ad@gmail.com>
2022-10-23 01:35:01 +0100

    portfolio mgt code

--- Shared/Models/Portfolio.swift
+++ Shared/Models/Portfolio.swift
@@ -8,6 +8,7 @@
 import Foundation
 
 enum PortfolioError : Error {
+    case locoCannotBeFound
     case locoCannotBeActive
     case locoHasRusted
     case alreadyOwnThisCard(card: Card)
@@ -28,10 +29,57 @@ final class Portfolio {
 }
 
 extension Portfolio {
-    func canAdd(card: Card) -> Bool {
-        return true
+    func canAdd(card: Card) -> Result<Bool, PortfolioError> {
+        do {
+            try validateAdd(card: card)
+            
+            return .success(true)
+        } catch let err {
+            return .failure(err as! PortfolioError)
+        }
     }
     
-    func add(card: Card) {
+    func add(card: Card) -> Result<Bool, PortfolioError> {
+        switch canAdd(card: card) {
+        case .success(_):
+            self._hand.append(card)
+            return .success(true)
+            
+        case .failure(let err):
+            return .failure(err as PortfolioError)
+        }
+    }
+    
+    
+    private func validateAdd(card: Card) throws {
+        guard let loco = card.loco else {
+            throw PortfolioError.locoCannotBeFound
+        }
+        guard loco.rust != .rusted else {
+            throw PortfolioError.locoHasRusted
+        }
+        guard loco.rust != .active else {
+            throw PortfolioError.locoCannotBeActive
+        }
+        if let _ = seek(card: card) {
+            throw PortfolioError.alreadyOwnThisCard(card: card)
+        }
+        if let _ = seekCardWithSameGeneration(as: card) {
+            throw PortfolioError.cannotOwnCardFromSameDeck
+        }
+    }
+    
+    
+    private func seek(card: Card) -> Card? {
+        return self.hand.filter { item in
+            return (card == item)
+        }.first
+    }
+    
+    private func seekCardWithSameGeneration(as card: Card) -> Card? {
+        let results = self.hand.filter { item in
+            return (card.loco!.generation ==  item.loco!.generation)
+        }.first
+        return results
     }
 }
