5e9152223ef785955597aaa87c295adc013b40f3
AD <silentflute.ad@gmail.com>
2022-10-30 21:54:02 +0000

    adding test for five players.

--- EngineYardTests/GameSetupTests.swift
+++ EngineYardTests/GameSetupTests.swift
@@ -55,7 +55,47 @@ final class GameSetupTests: XCTestCase {
                 XCTAssertTrue(firstCard.loco!.qty == players.count)
             }
             
+        } catch let err  {
+            XCTAssertThrowsError(err)
+        }
+    }
+    
+    func testSetupForFivePlayers() throws {
+        let seedCash = Constants.NumberOfPlayers.seedCashFivePlayer
+        let players = [Player(), Player(), Player()]
+        var game = Game()
+        let gsm = GameSetupManager(game: game)
+        do {
+            game = try gsm.setup(for: players)
             
+            guard let hasPlayers = game.players else {
+                XCTAssertThrowsError("No players defined")
+                return
+            }
+            
+            XCTAssertTrue(hasPlayers.count == 3, "Got \(hasPlayers.count)")
+            
+            for p in players {
+                XCTAssertTrue(p.balance == seedCash)
+                XCTAssertTrue(p.hand.count == 1)
+                guard let firstCard = p.hand.first else {
+                    XCTAssertThrowsError("No card found")
+                    return
+                }
+                guard let firstLoco = firstCard.loco else {
+                    XCTAssertThrowsError("No loco found")
+                    return
+                }
+                
+                XCTAssertTrue(firstLoco.livery == .green)
+                XCTAssertTrue(firstLoco.generation == .first)
+                XCTAssertTrue(firstCard.production.units == 1)
+                XCTAssertTrue(firstCard.production.spent == 0)
+                XCTAssertTrue(firstCard.loco!.rust == .active)
+                XCTAssertTrue(firstCard.loco!.isAvailable)
+                XCTAssertFalse(firstCard.loco!.isFull)
+                XCTAssertTrue(firstCard.loco!.qty == players.count)
+            }
             
         } catch let err  {
             XCTAssertNoThrow(err)
