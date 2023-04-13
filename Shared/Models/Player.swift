1881f27007b9bfb69dab2e5b7f10106467d87672
AD <silentflute.ad@gmail.com>
2023-04-13 13:22:40 +0100

    Adding a basic class for the player

---
+++ Shared/Models/Player.swift
@@ -0,0 +1,18 @@
+//
+//  Player.swift
+//  EngineYardTests
+//
+//  Created by Amarjit on 13/04/2023.
+//
+
+import Foundation
+
+class Player {
+    var cash : Int
+    var icon : String?
+    
+    init(cash: Int = 0, icon: String? = nil) {
+        self.cash = cash
+        self.icon = icon
+    }
+}
