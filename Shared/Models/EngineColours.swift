dfe4aa4c16fd7206360df66da7b077991bb88bfe
AD <silentflute.ad@gmail.com>
2023-04-13 11:17:49 +0100

    Create file for Generations and EngineColors

---
+++ Shared/Models/EngineColours.swift
@@ -0,0 +1,12 @@
+//
+//  EngineColours.swift
+//  EngineYard
+//
+//  Created by Amarjit on 13/04/2023.
+//
+
+import Foundation
+
+public enum EngineColor: Int {
+    case green = 1, red, yellow, blue
+}
---
+++ Shared/Models/Generations.swift
@@ -0,0 +1,14 @@
+//
+//  Generations.swift
+//  EngineYard
+//
+//  Created by Amarjit on 13/04/2023.
+//
+
+import Foundation
+
+public enum Generation: Int {
+    case first = 1, second, third, fourth, fifth
+}
+
+
