812f0a8cadbac7d63d452d36f3eb6dcd1ebe0939
AD <silentflute.ad@gmail.com>
2023-04-23 02:15:38 +0100

    add constants, a tax

--- Shared/Models/Constants.swift
+++ Shared/Models/Constants.swift
@@ -10,4 +10,53 @@ import Foundation
 // Game constants
 
 public struct Constants {
+    public static let taxRate: Float = 0.10
+    public static let endGameTriggerCash = 300 // need >=300 after tax to trigger end of game
+        
+    public struct NumberOfPlayers {
+        public static let min = 3
+        public static let max = 5
+        
+        func seedCash(players: Int) -> Int {
+            switch players {
+            case 3,4: return 12
+            case 5: return 14
+            default: return 0
+            }
+        }
+        
+        public static func isValid(_ playerCount: Int = NumberOfPlayers.min) -> Bool {
+            return (playerCount >= NumberOfPlayers.min && playerCount <= NumberOfPlayers.max)
+        }
+    }
+    
+    public static let totalLocos = 14
+    public static let totalDecks = 14
+    public static let totalCards = 43
+    public static let totalCapacity = 46 // for dice
+    
+    public struct Green {
+        public static let totalCards = 20
+        public static let totalGenerations = 5
+        public static let capacityPerGeneration = [3,4,4,5,5]
+        public static let qtyPerGeneration = [4,4,4,4,4]
+    }
+    public struct Red {
+        public static let totalCards = 13
+        public static let totalGenerations = 4
+        public static let capacityPerGeneration = [3,3,4,4,0]
+        public static let qtyPerGeneration = [3,3,3,4,0]
+    }
+    public struct Yellow {
+        public static let totalCards = 7
+        public static let totalGenerations = 3
+        public static let capacityPerGeneration = [2,3,3,0,0]
+        public static let qtyPerGeneration = [2,2,3,0,0]
+    }
+    public struct Blue {
+        public static let totalCards = 3
+        public static let totalGenerations = 2
+        public static let capacityPerGeneration = [1,2,0,0,0]
+        public static let qtyPerGeneration = [1,2,0,0,0]
+    }
 }
---
+++ Shared/Models/Tax.swift
@@ -0,0 +1,41 @@
+//
+//  Tax.swift
+//  EngineYard
+//
+//  Created by Amarjit on 23/04/2023.
+//
+
+import Foundation
+
+internal protocol TaxProtocol {
+    static func calculate(on amount: Int) -> Int
+    static func pay(on amount: Int) -> Int
+    static func add(on amount: Int) -> Int
+}
+
+struct Tax : TaxProtocol {
+    // Players pay 10% of their cash in taxes, rounded down
+    static var rate: Float {
+        return Constants.taxRate
+    }
+
+    // Finds out how much tax is due on an amount
+    // Return rounded down amount as per game rules
+    static func calculate(on amount: Int) -> Int {
+        guard (amount > 0) else { return 0 }
+        return Int(floor(Float(amount) * Tax.rate))
+    }
+
+    // Used to pay tax on an amount
+    static func pay(on amount: Int) -> Int {
+        guard (amount > 0) else { return 0 }
+        return Int(amount - calculate(on: amount))
+    }
+
+    // Used to add a "sales tax"
+    static func add(on amount: Int) -> Int {
+        guard (amount > 0) else { return 0 }
+        let taxDue: Int = calculate(on: amount)
+        return Int(amount + taxDue)
+    }
+}
