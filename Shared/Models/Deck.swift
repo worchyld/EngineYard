80996c3019cf9f62753a2a759be9ffd3e999fd85
AD <silentflute.ad@gmail.com>
2023-05-01 09:27:42 +0100

    remove protocol as its not needed,, keep it simple

--- Shared/Models/Deck.swift
+++ Shared/Models/Deck.swift
@@ -8,22 +8,11 @@
 import Foundation
 
 // Generic deck
-
-protocol DeckDelegate: AnyObject {
-    associatedtype Element
-    var size: Int { get }
-    var isEmpty: Bool { get }
-    var cards:[Element] { get set }
-    func push(_ element: Element)
-    func pop(_ card: Card) -> Element?
-    func find(_ card: Element) -> Int?// where Element: Equatable
-    func shuffle()
-}
-
 class Deck<Element> {
     var size: Int {
         return cards.count
     }
+    
     var isEmpty: Bool {
         return (cards.count == 0)
     }
@@ -34,15 +23,15 @@ class Deck<Element> {
         cards.append(element)
     }
     
-    func find(_ card: Element) -> Int? where Element : Equatable {
-        return cards.firstIndex(of: card)
-    }
-    
     func pop(_ card: Element) -> Element? where Element: Equatable {
         guard let index = find(card) else {
             return nil
         }
         return cards.remove(at: index)
+    }
+    
+    func find(_ card: Element) -> Int? where Element: Equatable {
+        cards.firstIndex(of: card)
     }
     
     func shuffle() {
