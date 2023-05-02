83b22c45903f49ce573b4b5e03c0b954ed702f72
AD <silentflute.ad@gmail.com>
2023-05-02 22:10:40 +0100

    Make a view for a single player hud

--- EngineYard/PlayerHUDView.swift
+++ EngineYard/PlayerHUDView.swift
@@ -8,13 +8,33 @@
 import SwiftUI
 
 struct PlayerHUDView: View {
+    let player: PlayerViewModel
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+            VStack(alignment: .leading, spacing: 8.0) {
+                AvatarView(avatar: player.avatar)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 8.0)
+                            .stroke(Color.blue, lineWidth: 2)
+                    )
+                
+                VStack(alignment: .leading, spacing: 5, content: {
+                    Text(cashFormat( amount: player.cash))
+                        .font(.caption)
+                        .fontWeight(.bold)
+                        .multilineTextAlignment(.center)
+
+                    Text("🚂 \(player.trains)")
+                        .font(.caption)
+                        .fontWeight(.semibold)
+                        .multilineTextAlignment(.center)
+                })
+            }
+        }
 }
 
 struct PlayerHUDView_Previews: PreviewProvider {
     static var previews: some View {
-        PlayerHUDView()
+        PlayerHUDView(player: PlayerViewModel(id: UUID(), avatar: "avt-1", cash: 330, trains: 14, active: true, hand: nil))
     }
 }
