4952db8c428ae5acbbf243961ddfe7aa4e0a9a02
AD <silentflute.ad@gmail.com>
2023-05-08 20:37:49 +0100

    add main menu swiftui file, outline only

---
+++ EngineYard/MainMenuView.swift
@@ -0,0 +1,27 @@
+//
+//  MainMenuView.swift
+//  EngineYard
+//
+//  Created by Amarjit on 08/05/2023.
+//
+
+import SwiftUI
+
+struct MainMenuView: View {
+    var body: some View {
+        HStack(alignment: .center, spacing: 20, content: {
+            Button("Play") {
+                
+            }
+            Button("Leaderboard") {
+                
+            }
+        })
+    }
+}
+
+struct MainMenuView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainMenuView()
+    }
+}
--- EngineYard/TrainListView.swift
+++ EngineYard/TrainListView.swift
@@ -26,6 +26,8 @@ struct TrainListView_Previews: PreviewProvider {
     static var previews: some View {
         TrainListView(
             locomotives: [
+                LocomotiveViewModel(with: Locomotive(id: UUID(), name: "Green-1", colour: .green, generation: .first, cost: 4, trainPool: 4, capacity: 3, rust: .notBuilt, orders: [Int](), sales: [Int](), initialOrder: nil)),
+                LocomotiveViewModel(with: Locomotive(id: UUID(), name: "Green-1", colour: .green, generation: .first, cost: 4, trainPool: 4, capacity: 3, rust: .notBuilt, orders: [Int](), sales: [Int](), initialOrder: nil)),
                 LocomotiveViewModel(with: Locomotive(id: UUID(), name: "Green-1", colour: .green, generation: .first, cost: 4, trainPool: 4, capacity: 3, rust: .notBuilt, orders: [Int](), sales: [Int](), initialOrder: nil))
             ]
         )
