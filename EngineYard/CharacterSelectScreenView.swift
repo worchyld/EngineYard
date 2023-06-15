5b630d0248bad0eab3563e0dd48303287a24d9cf
AD <silentflute.ad@gmail.com>
2023-06-15 14:59:37 +0100

    Made a simple character select screen view

--- EngineYard/CharacterSelectScreenView.swift
+++ EngineYard/CharacterSelectScreenView.swift
@@ -8,13 +8,68 @@
 import SwiftUI
 
 struct CharacterSelectScreenView: View {
+    let images: [String]
+    @State private var selectedButton: Int = 0
+    
     var body: some View {
-        Text("hello world")
+        HStack(alignment: .firstTextBaseline, spacing: 18, content: {
+            VStack() {
+                HStack() {
+                    VStack() {
+                        Text("Engine yard").font(.largeTitle)
+                        Text("Choose your character").font(.largeTitle)
+                    }
+                }
+                HStack (spacing: 5) {
+                    //ForEach(avatars) { avt in
+                    ForEach(0..<images.count) { index in
+                        let img = images[index]
+                        
+                        Button(action: {
+                            selectedButton = index
+                        }) {
+                            AvatarView(avatar: img).overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(selectedButton == index ? Color.blue : Color.clear, lineWidth: 4)
+                            )                            
+                        }
+                    }
+                    
+                    
+                }
+                VStack {
+                    
+                    Button(action: {
+                                // Action to perform when the button is clicked
+                                print("Button clicked!")
+                            }) {
+                                Text("Start")
+                                    .foregroundColor(.blue)
+                                    .padding()
+                                    .overlay(
+                                        RoundedRectangle(cornerRadius: 10)
+                                            .stroke(Color.blue, lineWidth: 2)
+                                    )
+                                    .padding(.horizontal, 20)
+                            }
+                }
+            }
+
+        })
     }
 }
 
 struct CharacterSelectScreenView_Previews: PreviewProvider {
     static var previews: some View {
-        CharacterSelectScreenView()
+        
+        let images = [
+            "avt-1",
+            "avt-2",
+            "avt-3",
+            "avt-4",
+            "avt-5"
+        ]
+        
+        CharacterSelectScreenView(images: images)
     }
 }
